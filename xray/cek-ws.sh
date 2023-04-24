9f96ec88fcf771a94cc8f5758c6f3d5944940d0a
arfprsty810 <94728173+arfprsty810@users.noreply.github.com>
2023-04-24 18:48:21 +0700

    Add files via upload

--- xray/cek-ws.sh
+++ xray/cek-ws.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
-
 red='\e[1;31m'
 green='\e[0;32m'
+yell='\e[1;33m'
+tyblue='\e[1;36m'
 NC='\e[0m'
+purple() { echo -e "\\033[35;1m${*}\\033[0m"; }
+tyblue() { echo -e "\\033[36;1m${*}\\033[0m"; }
+yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
 green() { echo -e "\\033[32;1m${*}\\033[0m"; }
 red() { echo -e "\\033[31;1m${*}\\033[0m"; }
-
 clear
 echo -n > /tmp/other.txt
 data=( `cat /etc/xray/config.json | grep '^#!' | cut -d ' ' -f 2 | sort | uniq`);
 
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\\E[0;41;36m    Xray Vmess User Login         \E[0m"
+echo -e "\\E[0;41;36m         Vmess User Login         \E[0m"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 
 for akun in "${data[@]}"

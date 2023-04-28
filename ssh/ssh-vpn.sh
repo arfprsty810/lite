3a78c192d0acee194ded514526c98849cde07343
arfprsty810 <94728173+arfprsty810@users.noreply.github.com>
2023-04-28 18:38:21 +0700

    Add files via upload

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -130,21 +130,21 @@ echo "neofetch" >> .profile
 clear
 
 # install webserver
-#apt -y install nginx php php-fpm php-cli php-mysql libxml-parser-perl
-#rm /etc/nginx/sites-enabled/default
-#rm /etc/nginx/sites-available/default
-#curl https://raw.githubusercontent.com/arfprsty810/lite/main/ssh/nginx.conf > /etc/nginx/nginx.conf
-#curl https://raw.githubusercontent.com/arfprsty810/lite/main/ssh/vps.conf > /etc/nginx/conf.d/vps.conf
-#sed -i 's/listen = \/var\/run\/php-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php/fpm/pool.d/www.conf
-#useradd -m vps;
-#mkdir -p /home/vps/public_html
-#echo "<?php phpinfo() ?>" > /home/vps/public_html/info.php
-#chown -R www-data:www-data /home/vps/public_html
-#chmod -R g+rw /home/vps/public_html
-#cd /home/vps/public_html
-#wget -O /home/vps/public_html/index.html "https://raw.githubusercontent.com/arfprsty810/lite/main/ssh/index.html1"
-#/etc/init.d/nginx restart
-#cd
+apt -y install nginx php php-fpm php-cli php-mysql libxml-parser-perl
+rm /etc/nginx/sites-enabled/default
+rm /etc/nginx/sites-available/default
+curl https://raw.githubusercontent.com/arfprsty810/lite/main/ssh/nginx.conf > /etc/nginx/nginx.conf
+curl https://raw.githubusercontent.com/arfprsty810/lite/main/ssh/vps.conf > /etc/nginx/conf.d/vps.conf
+sed -i 's/listen = \/var\/run\/php-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php/fpm/pool.d/www.conf
+useradd -m vps;
+mkdir -p /home/vps/public_html
+echo "<?php phpinfo() ?>" > /home/vps/public_html/info.php
+chown -R www-data:www-data /home/vps/public_html
+chmod -R g+rw /home/vps/public_html
+cd /home/vps/public_html
+wget -O /home/vps/public_html/index.html "https://raw.githubusercontent.com/arfprsty810/lite/main/ssh/index.html1"
+/etc/init.d/nginx restart
+cd
 
 # install badvpn
 cd

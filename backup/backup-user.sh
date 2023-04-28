2fa359c3e6d4542ca65146598fca8cb7bba9f854
arfprsty810 <94728173+arfprsty810@users.noreply.github.com>
2023-04-28 10:50:35 +0700

    Add files via upload

--- backup/backup-user.sh
+++ backup/backup-user.sh
@@ -11,50 +11,39 @@ green() { echo -e "\\033[32;1m${*}\\033[0m"; }
 red() { echo -e "\\033[31;1m${*}\\033[0m"; }
 clear
 
-# --- delete  akun --- #
+# --- delete akun --- #
 # vless
 datavl=( `cat /etc/xray/config.json | grep '#vl#' | cut -d ' ' -f 2 | sort | uniq`);
 for akun in "${datavl[@]}"
 do
-exp=$(grep -wE "^#& $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
-sed -i "/^#& $akun $exp/,/^},{/d" /etc/xray/config.json
+if [[ -z "$akun" ]]; then
+akun="tidakada"
+fi
+exp=$(grep -wE "^#vl# $akun" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
+sed -i "/^#vl# $akun $exp/,/^},{/d" /etc/xray/config.json
 done
 
 # vmess
 datavm=( `cat /etc/xray/config.json | grep '#vm#' | cut -d ' ' -f 2 | sort | uniq`);
 for akun in "${datavm[@]}"
 do
-exp=$(grep -wE "^#& $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
-sed -i "/^#& $akun $exp/,/^},{/d" /etc/xray/config.json
+if [[ -z "$akun" ]]; then
+akun="tidakada"
+fi
+exp=$(grep -wE "^#vm# $akun" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
+sed -i "/^#vm# $akun $exp/,/^},{/d" /etc/xray/config.json
 done
 
 # trojan
 datatr=( `cat /etc/xray/config.json | grep '#tr#' | cut -d ' ' -f 2 | sort | uniq`);
 for akun in "${datatr[@]}"
 do
-exp=$(grep -wE "^#& $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
-sed -i "/^#& $akun $exp/,/^},{/d" /etc/xray/config.json
+if [[ -z "$akun" ]]; then
+akun="tidakada"
+fi
+exp=$(grep -wE "^#tr# $akun" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
+sed -i "/^#tr# $akun $exp/,/^},{/d" /etc/xray/config.json
 done
-
-# --- tambah akun --- #
-# vless
-AKUN_VLESS_1="vless_asu"
-UID_VLESS_1="6ab17040-6eef-45ee-864d-36f4c758241d"
-EXP_VLESS_1="2024-03-23"
-sed -i '/#vless$/a\#vl# '"$AKUN_VLESS_1 $EXP_VLESS_1"'\
-},{"id": "'""$UID_VLESS_1""'","email": "'""$AKUN_VLESS_1""'"' /etc/xray/config.json
-sed -i '/#vlessgrpc$/a\#vl# '"$AKUN_VLESS_1 $exp"'\
-},{"id": "'""$UID_VLESS_1""'","email": "'""$AKUN_VLESS_1""'"' /etc/xray/config.json
-
-# vmess
-AKUN_VMESS_1="vmess_asu"
-UID_VMESS_1="f1a53fef-8533-457b-ab2f-aa915f167519"
-EXP_VMESS_1="2024-03-23"
-sed -i '/#vmess$/a\#vl# '"$AKUN_VMESS_1 $EXP_VMESS_1"'\
-},{"id": "'""$UID_VMESS_1""'","email": "'""$AKUN_VMESS_1""'"' /etc/xray/config.json
-sed -i '/#vmessgrpc$/a\#vl# '"$AKUN_VMESS_1 $EXP_VMESS_1"'\
-},{"id": "'""$UID_VMESS_1""'","email": "'""$AKUN_VMESS_1""'"' /etc/xray/config.json
-
 
 systemctl restart xray
 sleep 1

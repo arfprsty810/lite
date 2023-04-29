e913542ddae749a8150436a2683fe5e77f0acb16
arfprsty810 <94728173+arfprsty810@users.noreply.github.com>
2023-04-29 20:36:27 +0700

    Add files via upload

--- backup/renew-config.sh
+++ backup/renew-config.sh
@@ -712,6 +712,13 @@ mkdir $logtrgo
 touch $trgo/akun.conf
 touch $logtrgo/trojan-go.log
 
+# Trojan Go Uuid
+cat /proc/sys/kernel/random/uuid > $trgo/uuid
+uuidtrgo=$(cat $trgo/uuid)
+#cat > $trgo/uuid << END
+#$uuid
+#END
+
 # Buat Config Trojan Go
 cat > $trgo/config.json << END
 {
@@ -723,7 +730,7 @@ cat > $trgo/config.json << END
   "log_level": 1,
   "log_file": "$logtrgo/trojan-go.log",
   "password": [
-      "$uuid"
+      "$uuidtrgo"
   ],
   "disable_http_check": true,
   "udp_timeout": 60,
@@ -795,11 +802,6 @@ RestartPreventExitStatus=23
 
 [Install]
 WantedBy=multi-user.target
-END
-
-# Trojan Go Uuid
-cat > $trgo/uuid << END
-$uuid
 END
 
 # restart

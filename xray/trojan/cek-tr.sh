3cc12040c18a069f03c3cb34d4b2beea915413aa
arfprsty810 <94728173+arfprsty810@users.noreply.github.com>
2023-04-29 13:58:52 +0700

    Add files via upload

--- xray/trojan/cek-tr.sh
+++ xray/trojan/cek-tr.sh
@@ -10,28 +10,9 @@ yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
 green() { echo -e "\\033[32;1m${*}\\033[0m"; }
 red() { echo -e "\\033[31;1m${*}\\033[0m"; }
 
-xray="/etc/arf/xray"
-trgo="/etc/arf/trojango"
-ipvps="/var/lib/arf"
-log="/var/log/arf/xray"
-logtrgo="/var/log/arf/trojango"
-# set random pwd
-openssl rand -base64 16 > $xray/passwd
-pwd=$(cat $xray/passwd)
-# set random uuid
-uuid=$(cat /proc/sys/kernel/random/uuid)
-
-clear
-source $ipvps/ipvps.conf
-if [[ "$IP" = "" ]]; then
-domain=$(cat $xray/domain)
-else
-domain=$IP
-fi
-
 clear
 echo -n > /tmp/other.txt
-data=( `cat $xray/config.json | grep '#tr#' | cut -d ' ' -f 2 | sort | uniq`);
+data=( `cat /etc/xray/config.json | grep '#tr#' | cut -d ' ' -f 2 | sort | uniq`);
 echo "------------------------------------";
 echo "-----=[ XRAY User Login ]=-----";
 echo "------------------------------------";
@@ -40,67 +21,34 @@ do
 if [[ -z "$akun" ]]; then
 akun="tidakada"
 fi
-echo -n > /tmp/iptr.txt
-data2=( `cat $log/access.log | tail -n 500 | cut -d " " -f 3 | sed 's/tcp://g' | cut -d ":" -f 1 | sort | uniq`);
-
+echo -n > /tmp/ipxray.txt
+data2=( `cat /var/log/xray/access.log | tail -n 500 | cut -d " " -f 3 | sed 's/tcp://g' | cut -d ":" -f 1 | sort | uniq`);
 for ip in "${data2[@]}"
 do
-jum=$(cat $log/access.log | grep -w "$akun" | tail -n 500 | cut -d " " -f 3 | sed 's/tcp://g' | cut -d ":" -f 1 | grep -w "$ip" | sort | uniq)
+jum=$(cat /var/log/xray/access.log | grep -w "$akun" | tail -n 500 | cut -d " " -f 3 | sed 's/tcp://g' | cut -d ":" -f 1 | grep -w "$ip" | sort | uniq)
 if [[ "$jum" = "$ip" ]]; then
-echo "$jum" >> /tmp/iptr.txt
+echo "$jum" >> /tmp/ipxray.txt
 else
 echo "$ip" >> /tmp/other.txt
 fi
-jum2=$(cat /tmp/iptr.txt)
+jum2=$(cat /tmp/ipxray.txt)
 sed -i "/$jum2/d" /tmp/other.txt > /dev/null 2>&1
 done
-
-jum=$(cat /tmp/iptr.txt)
+jum=$(cat /tmp/ipxray.txt)
 if [[ -z "$jum" ]]; then
 echo > /dev/null
 else
-jum2=$(cat /tmp/iptr.txt | nl)
-lastlogin=$(cat $log/access.log | grep -w "$akun" | tail -n 500 | cut -d " " -f 2 | tail -1)
-
-#trojango
-echo -n > /tmp/other.txt
-datatr=( `cat $trgo/akun.conf | grep '^#tr#' | cut -d ' ' -f 2`);
-for akun in "${datatr[@]}"
-do
-if [[ -z "$akun" ]]; then
-akun="tidakada"
-fi
-echo -n > /tmp/iptr.txt
-datatr2=( `netstat -anp | grep ESTABLISHED | grep tcp6 | grep trojan-go | awk '{print $5}' | cut -d: -f1 | sort | uniq`);
-
-for ip in "${datatr2[@]}"
-do
-jumtr=$(cat $logtrgo/trojan-go.log | grep -w $akun | awk '{print $3}' | cut -d: -f1 | grep -w $ip | sort | uniq)
-if [[ "$jumtr" = "$ip" ]]; then
-echo "$jumtr" >> /tmp/iptr.txt
-else
-echo "$ip" >> /tmp/other.txt
-fi
-jumtr2=$(cat /tmp/iptr.txt)
-sed -i "/$jumtr2/d" /tmp/other.txt > /dev/null 2>&1
-done
-
-jumtr=$(cat /tmp/iptr.txt)
-if [[ -z "$jumtr" ]]; then
-echo > /dev/null
-else
-jumtr2=$(cat /tmp/iptr.txt | nl)
-lastlogintr=$(cat $logtrgo/access.log | grep -w "$akun" | tail -n 500 | cut -d " " -f 2 | tail -1)
-
+jum2=$(cat /tmp/ipxray.txt | nl)
+lastlogin=$(cat /var/log/xray/access.log | grep -w "$akun" | tail -n 500 | cut -d " " -f 2 | tail -1)
 echo -e "user :${GREEN} ${akun} ${NC}
 ${RED}Online Jam ${NC}: ${lastlogin} wib";
 echo -e "$jum2";
 echo "-------------------------------"
 fi
-rm -rf /tmp/iptr.txt
+rm -rf /tmp/ipxray.txt
+done
 rm -rf /tmp/other.txt
-done
-done
+
 echo ""
 read -n 1 -s -r -p "Press any key to back on menu"
 menu
--- xray/trojan/renew-tr.sh
+++ xray/trojan/renew-tr.sh
@@ -10,26 +10,7 @@ yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
 green() { echo -e "\\033[32;1m${*}\\033[0m"; }
 red() { echo -e "\\033[31;1m${*}\\033[0m"; }
 
-xray="/etc/arf/xray"
-trgo="/etc/arf/trojango"
-ipvps="/var/lib/arf"
-log="/var/log/arf/xray"
-logtrgo="/var/log/arf/trojango"
-# set random pwd
-openssl rand -base64 16 > $xray/passwd
-pwd=$(cat $xray/passwd)
-# set random uuid
-uuid=$(cat /proc/sys/kernel/random/uuid)
-
-clear
-source $ipvps/ipvps.conf
-if [[ "$IP" = "" ]]; then
-domain=$(cat $xray/domain)
-else
-domain=$IP
-fi
-
-NUMBER_OF_CLIENTS=$(grep -c -E "^#tr# " "$xray/config.json")
+NUMBER_OF_CLIENTS=$(grep -c -E "^#tr# " "/etc/xray/config.json")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 		clear
         echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
@@ -49,7 +30,7 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^#tr# " "$xray/config.json")
     echo -e "\\E[0;41;36m            Renew Trojan             \E[0m"
     echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
     echo ""
-  	grep -E "^#tr# " "$xray/config.json" | cut -d ' ' -f 2-3 | column -t | sort | uniq
+  	grep -E "^#tr# " "/etc/xray/config.json" | cut -d ' ' -f 2-3 | column -t | sort | uniq
     echo ""
     red "tap enter to go back"
     echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
@@ -58,14 +39,14 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^#tr# " "$xray/config.json")
     menu
     else
     read -p "Expired (days): " masaaktif
-    exp=$(grep -wE "^#tr# $user" "$xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
+    exp=$(grep -wE "^#tr# $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
     now=$(date +%Y-%m-%d)
     d1=$(date -d "$exp" +%s)
     d2=$(date -d "$now" +%s)
     exp2=$(( (d1 - d2) / 86400 ))
     exp3=$(($exp2 + $masaaktif))
     exp4=`date -d "$exp3 days" +"%Y-%m-%d"`
-    sed -i "/#tr# $user/c\#tr# $user $exp4" $xray/config.json
+    sed -i "/#tr# $user/c\#tr# $user $exp4" /etc/xray/config.json
     systemctl restart xray > /dev/null 2>&1
     clear
     echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"

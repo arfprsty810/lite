a4a034d07d5ed6c31da24acfd13d60dda41d1e19
arfprsty810 <94728173+arfprsty810@users.noreply.github.com>
2023-04-29 18:24:35 +0700

    Add files via upload

--- xray/trojan/cek-tr.sh
+++ xray/trojan/cek-tr.sh
@@ -23,44 +23,6 @@ domain=$IP
 fi
 
 clear
-echo -n > /tmp/other.txt
-data=( `cat /etc/xray/config.json | grep '#tr#' | cut -d ' ' -f 2 | sort | uniq`);
-echo "------------------------------------";
-echo "-----=[ Trojan-WS & Trojan-GRPC User Login ]=-----";
-echo "------------------------------------";
-for akun in "${data[@]}"
-do
-if [[ -z "$akun" ]]; then
-akun="tidakada"
-fi
-echo -n > /tmp/ipxray.txt
-data2=( `cat /var/log/xray/access.log | tail -n 500 | cut -d " " -f 3 | sed 's/tcp://g' | cut -d ":" -f 1 | sort | uniq`);
-for ip in "${data2[@]}"
-do
-jum=$(cat /var/log/xray/access.log | grep -w "$akun" | tail -n 500 | cut -d " " -f 3 | sed 's/tcp://g' | cut -d ":" -f 1 | grep -w "$ip" | sort | uniq)
-if [[ "$jum" = "$ip" ]]; then
-echo "$jum" >> /tmp/ipxray.txt
-else
-echo "$ip" >> /tmp/other.txt
-fi
-jum2=$(cat /tmp/ipxray.txt)
-sed -i "/$jum2/d" /tmp/other.txt > /dev/null 2>&1
-done
-jum=$(cat /tmp/ipxray.txt)
-if [[ -z "$jum" ]]; then
-echo > /dev/null
-else
-jum2=$(cat /tmp/ipxray.txt | nl)
-lastlogin=$(cat /var/log/xray/access.log | grep -w "$akun" | tail -n 500 | cut -d " " -f 2 | tail -1)
-echo -e "user :${GREEN} ${akun} ${NC}
-${RED}Online Jam ${NC}: ${lastlogin} wib";
-echo -e "$jum2";
-echo "-------------------------------"
-fi
-rm -rf /tmp/ipxray.txt
-done
-rm -rf /tmp/other.txt
-
 
 echo -n > /tmp/other.txt
 datatrgo=( `cat $trgo/akun.conf | grep '^#trgo#' | cut -d ' ' -f 2 | sort | uniq`);
@@ -74,7 +36,6 @@ akun="tidakada"
 fi
 echo -n > /tmp/iptrojango.txt
 datatrgo2=( `cat $logtrgo/trojan-go.log | tail -n 500 | cut -d " " -f 3 | sed 's/tcp://g' | cut -d ":" -f 1 | sort | uniq`);
-
 for ip in "${datatrgo2[@]}"
 do
 jumtrgo=$(cat $logtrgo/trojan-go.log | grep -w "$akun" | tail -n 500 | cut -d " " -f 3 | sed 's/tcp://g' | cut -d ":" -f 1 | grep -w "$ip" | sort | uniq)

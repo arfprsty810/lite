50fc747b585fef639196fc11a3ce487047be928a
arfprsty810 <94728173+arfprsty810@users.noreply.github.com>
2023-05-01 21:20:44 +0700

    Add files via upload

--- xray/restart.sh
+++ xray/restart.sh
@@ -39,8 +39,7 @@ screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 1000
 systemctl restart rc-local.service
 clear
 cat /root/log-install.txt
-echo -e "      All Service/s Successfully Restarted         "
-echo ""
+echo "      All Service/s Successfully Restarted         "
 echo ""
 read -n 1 -s -r -p "Press any key to back on menu"
 clear

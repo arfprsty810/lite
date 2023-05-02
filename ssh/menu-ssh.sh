4ecfde2518823f2bee42dc9cd5dace1c5c64e381
arfprsty810 <94728173+arfprsty810@users.noreply.github.com>
2023-05-02 23:34:21 +0700

    Add files via upload

--- ssh/menu-ssh.sh
+++ ssh/menu-ssh.sh
@@ -1,1 +1,103 @@
+#!/bin/bash
+# Menu For Script
+# Edition : Stable Edition V1.0
+# (C) Copyright 2021-2022
+# =========================================
+BIBlack='\033[1;90m'      # Black
+BIRed='\033[1;91m'        # Red
+BIGreen='\033[1;92m'      # Green
+BIYellow='\033[1;93m'     # Yellow
+BIBlue='\033[1;94m'       # Blue
+BIPurple='\033[1;95m'     # Purple
+BICyan='\033[1;96m'       # Cyan
+BIWhite='\033[1;97m'      # White
+UWhite='\033[4;37m'       # White
+On_IPurple='\033[0;105m'  #
+On_IRed='\033[0;101m'
+IBlack='\033[0;90m'       # Black
+IRed='\033[0;91m'         # Red
+IGreen='\033[0;92m'       # Green
+IYellow='\033[0;93m'      # Yellow
+IBlue='\033[0;94m'        # Blue
+IPurple='\033[0;95m'      # Purple
+ICyan='\033[0;96m'        # Cyan
+IWhite='\033[0;97m'       # White
+NC='\e[0m'
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
 
+# // Export Color & Information
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export YELLOW='\033[0;33m'
+export BLUE='\033[0;34m'
+export PURPLE='\033[0;35m'
+export CYAN='\033[0;36m'
+export LIGHT='\033[0;37m'
+export NC='\033[0m'
+
+# // Export Banner Status Information
+export EROR="[${RED} EROR ${NC}]"
+export INFO="[${YELLOW} INFO ${NC}]"
+export OKEY="[${GREEN} OKEY ${NC}]"
+export PENDING="[${YELLOW} PENDING ${NC}]"
+export SEND="[${YELLOW} SEND ${NC}]"
+export RECEIVE="[${YELLOW} RECEIVE ${NC}]"
+
+# // Export Align
+export BOLD="\e[1m"
+export WARNING="${RED}\e[5m"
+export UNDERLINE="\e[4m"
+
+# // Exporting URL Host
+export Server_URL="raw.githubusercontent.com/wunuit/test/main"
+export Server1_URL="raw.githubusercontent.com/wunuit/limit/main"
+export Server_Port="443"
+export Server_IP="underfined"
+export Script_Mode="Stable"
+export Auther=".geovpn"
+
+# // Root Checking
+if [ "${EUID}" -ne 0 ]; then
+		echo -e "${EROR} Please Run This Script As Root User !"
+		exit 1
+fi
+
+# // Exporting IP Address
+export IP=$( curl -s https://ipinfo.io/ip/ )
+
+# // Exporting Network Interface
+export NETWORK_IFACE="$(ip route show to default | awk '{print $5}')"
+
+menu
+}
+clear
+echo -e "${BICyan} ┌─────────────────────────────────────────────────────┐${NC}"
+echo -e "       ${BIWhite}${UWhite}SSH ${NC}"
+echo -e ""
+echo -e "     ${BICyan}[${BIWhite}1${BICyan}] Add Account SSH      "
+echo -e "     ${BICyan}[${BIWhite}2${BICyan}] Delete Account SSH      "
+echo -e "     ${BICyan}[${BIWhite}3${BICyan}] Renew Account SSH      "
+echo -e "     ${BICyan}[${BIWhite}4${BICyan}] Cek User SSH     "
+echo -e "     ${BICyan}[${BIWhite}5${BICyan}] Mullog SSH     "
+echo -e "     ${BICyan}[${BIWhite}6${BICyan}] Auto Del user Exp     "
+echo -e "     ${BICyan}[${BIWhite}7${BICyan}] Auto Kill user SSH    "
+echo -e "     ${BICyan}[${BIWhite}8${BICyan}] Cek Member SSH"
+echo -e " ${BICyan}└─────────────────────────────────────────────────────┘${NC}"
+echo -e "     ${BIYellow}Press x or [ Ctrl+C ] • To-${BIWhite}Exit${NC}"
+echo ""
+read -p " Select menu : " opt
+echo -e ""
+case $opt in
+1) clear ; usernew ;;
+2) clear ; del ;;
+3) clear ; renew;;
+4) clear ; cek ;;
+5) clear ; ceklim ;;
+6) clear ; autodel ;;
+7) clear ; autokill ;;
+8) clear ; member ;;
+0) clear ; menu ;;
+x) exit ;;
+*) echo -e "" ; echo "Press any key to back on menu" ; sleep 1 ; menu ;;
+esac

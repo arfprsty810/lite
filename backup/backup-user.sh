8fb301897232ec1f6f6c24e10fb3ce6ade239fee
arfprsty810 <94728173+arfprsty810@users.noreply.github.com>
2023-05-02 11:35:40 +0700

    Add files via upload

--- backup/backup-user.sh
+++ backup/backup-user.sh
@@ -50,17 +50,39 @@ sed -i "/^#trgo# $akun $exp/d" /etc/trojan-go/akun.conf
 done
 clear
 
+datass=( `cat /etc/shadowsocks-libev/akun.conf | grep '#ss#' | cut -d ' ' -f 2 | sort | uniq`);
+for akun in "${datatr[@]}"
+do
+if [[ -z "$akun" ]]; then
+akun="tidakada"
+fi
+exp=$(grep -wE "^#ss# $akun" "/etc/shadowsocks-libev/akun.conf" | cut -d ' ' -f 3 | sort | uniq)
+sed -i "/^#ss# $akun $exp/,/^port_http/d" "/etc/shadowsocks-libev/akun.conf"
+service cron restart
+systemctl disable shadowsocks-libev-server@$akun-tls.service
+systemctl disable shadowsocks-libev-server@$akun-http.service
+systemctl stop shadowsocks-libev-server@$akun-tls.service
+systemctl stop shadowsocks-libev-server@$akun-http.service
+rm -f "/etc/shadowsocks-libev/$akun-tls.json"
+rm -f "/etc/shadowsocks-libev/$akun-http.json"
+done
+clear
+
 # -------------------------------------- tambah akun -------------------------------------- #
 # vmess
 AKUN_VMESS_1="vmess_arf"
 UID_VMESS_1="f1a53fef-8533-457b-ab2f-aa915f167519"
 EXP_VMESS_1="2030-01-01"
+# Vmess
 sed -i '/#vmess$/a\#vm# '"$AKUN_VMESS_1 $EXP_VMESS_1"'\
 },{"id": "'""$UID_VMESS_1""'","email": "'""$AKUN_VMESS_1""'"' /etc/xray/config.json
+# Vmess Worry
 sed -i '/#vmessworry$/a\#vm# '"$AKUN_VMESS_1 $EXP_VMESS_1"'\
 },{"id": "'""$UID_VMESS_1""'","alterId": '"0"',"email": "'""$AKUN_VMESS_1""'"' /etc/xray/config.json
+# Vmess Kuota
 sed -i '/#vmesskuota$/a\#vm# '"$AKUN_VMESS_1 $EXP_VMESS_1"'\
 },{"id": "'""$UID_VMESS_1""'","alterId": '"0"',"email": "'""$AKUN_VMESS_1""'"' /etc/xray/config.json
+# Vmess gRPC
 sed -i '/#vmessgrpc$/a\#vm# '"$AKUN_VMESS_1 $EXP_VMESS_1"'\
 },{"id": "'""$UID_VMESS_1""'","email": "'""$AKUN_VMESS_1""'"' /etc/xray/config.json
 clear
@@ -69,25 +91,85 @@ clear
 AKUN_VLESS_1="vless_arf"
 UID_VLESS_1="77f6e656-649b-4caf-9beb-6e9f32290aac"
 EXP_VLESS_1="2030-01-01"
+# Vless
 sed -i '/#vless$/a\#vl# '"$AKUN_VLESS_1 $EXP_VLESS_1"'\
 },{"id": "'""$UID_VLESS_1""'","email": "'""$AKUN_VLESS_1""'"' /etc/xray/config.json
+# Vless gRPC
 sed -i '/#vlessgrpc$/a\#vl# '"$AKUN_VLESS_1 $EXP_VLESS_1"'\
 },{"id": "'""$UID_VLESS_1""'","email": "'""$AKUN_VLESS_1""'"' /etc/xray/config.json
 clear
 
 # trojan
 AKUN_TROJAN_1="trojan_arf"
+PASSWORD_TROJAN_GO_1="trojan_arf"
 UID_TROJAN_1="8394ff55-d075-4d1d-a65a-c82f16dc62d1"
 UID_TROJAN_GO=$(cat /etc/trojan-go/uuid)
 EXP_TROJAN_1="2030-01-01"
+EXP_TROJAN_GO1="2030-01-01"
+# Trojan-WS
 sed -i '/#trojanws$/a\#tr# '"$AKUN_TROJAN_1 $EXP_TROJAN_1"'\
 },{"password": "'""$UID_TROJAN_1""'","email": "'""$AKUN_TROJAN_1""'"' /etc/xray/config.json
+# Trojan gRPC
 sed -i '/#trojangrpc$/a\#tr# '"$AKUN_TROJAN_1 $EXP_TROJAN_1"'\
 },{"password": "'""$UID_TROJAN_1""'","email": "'""$AKUN_TROJAN_1""'"' /etc/xray/config.json
-#sed -i '/"'""$UID_TROJAN_GO""'"$/a\,#trgo# '"$AKUN_TROJAN_1 $EXP_TROJAN_1"'\
-#"'""$UID_TROJAN_1""'"' /etc/trojan-go/config.json
-sed -i '/"'""$UID_TROJAN_GO""'"$/a\,"'""$AKUN_TROJAN_1""'"' /etc/trojan-go/config.json
-echo -e "#trgo# $AKUN_TROJAN_1 $EXP_TROJAN_1" >> /etc/trojan-go/akun.conf
+# Trojan-GO
+sed -i '/"'""$UID_TROJAN_GO""'"$/a\,"'""$PASSWORD_TROJAN_GO_1""'"' /etc/trojan-go/config.json
+echo -e "#trgo# $PASSWORD_TROJAN_GO_1 $EXP_TROJAN_GO1" >> /etc/trojan-go/akun.conf
+clear
+
+# ShadowSocks
+lastport1=$(grep "port_tls" /etc/shadowsocks-libev/akun.conf | tail -n1 | awk '{print $2}')
+lastport2=$(grep "port_http" /etc/shadowsocks-libev/akun.conf | tail -n1 | awk '{print $2}')
+if [[ $lastport1 == '' ]]; then
+tls=2443
+else
+tls="$((lastport1+1))"
+fi
+if [[ $lastport2 == '' ]]; then
+http=3443
+else
+http="$((lastport2+1))"
+fi
+METHOD="aes-256-cfb"
+AKUN_SS_1="ss_arf"
+PASSWORD_SS_1="ss_arf"
+EXP_SS_1="2030-01-01"
+cat > /etc/shadowsocks-libev/$akun-tls.json<<END
+{   
+    "server":"0.0.0.0",
+    "server_port":$tls,
+    "password":"$PASSWORD_SS_1",
+    "timeout":60,
+    "method":"$METHOD",
+    "fast_open":true,
+    "no_delay":true,
+    "nameserver":"8.8.8.8",
+    "mode":"tcp_and_udp",
+    "plugin":"obfs-server",
+    "plugin_opts":"obfs=tls"
+}
+END
+cat > /etc/shadowsocks-libev/$akun-http.json <<-END
+{
+    "server":"0.0.0.0",
+    "server_port":$http,
+    "password":"$PASSWORD_SS_1",
+    "timeout":60,
+    "method":"$METHOD",
+    "fast_open":true,
+    "no_delay":true,
+    "nameserver":"8.8.8.8",
+    "mode":"tcp_and_udp",
+    "plugin":"obfs-server",
+    "plugin_opts":"obfs=http"
+}
+END
+chmod +x /etc/shadowsocks-libev/$akun-tls.json
+chmod +x /etc/shadowsocks-libev/$akun-http.json
+echo -e "#ss# $PASSWORD_SS_1 $EXP_SS_1
+port_tls $tls
+port_http $http">>"/etc/shadowsocks-libev/akun.conf"
+service cron restart
 clear
 
 # -------------------------------------- restart service -------------------------------------- #
@@ -103,6 +185,27 @@ systemctl restart xray
 systemctl restart nginx
 systemctl enable runn
 systemctl restart runn
+systemctl enable shadowsocks-libev-server@$akun-tls.service
+systemctl start shadowsocks-libev-server@$akun-tls.service
+systemctl enable shadowsocks-libev-server@$akun-http.service
+systemctl start shadowsocks-libev-server@$akun-http.service
+systemctl stop trojan-go
+systemctl start trojan-go
+systemctl enable trojan-go
+systemctl restart trojan-go
+#systemctl restart ws-dropbear.service >/dev/null 2>&1
+#systemctl restart ws-stunnel.service >/dev/null 2>&1
+systemctl restart xray.service >/dev/null 2>&1
+/etc/init.d/ssh restart
+/etc/init.d/dropbear restart
+#/etc/init.d/stunnel5 restart
+/etc/init.d/fail2ban restart
+/etc/init.d/cron restart
+/etc/init.d/nginx restart
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 1000
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 1000
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 1000
+systemctl restart rc-local.service
 clear
 
 echo ""

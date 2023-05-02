7ba8ff563162e20171fbd0f8c09c9012aa8eba62
arfprsty810 <94728173+arfprsty810@users.noreply.github.com>
2023-05-02 20:24:38 +0700

    Add files via upload

---
+++ ssh/delssh.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# 
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+
+read -p "Username SSH to Delete : " Pengguna
+
+if getent passwd $Pengguna > /dev/null 2>&1; then
+        userdel $Pengguna
+        echo -e "Username $Pengguna Telah Di Hapus"
+else
+        echo -e "Failure: Username $Pengguna Tidak Ada"
+fi
+read -n 1 -s -r -p "Press any key to back on menu"
+clear
+menu

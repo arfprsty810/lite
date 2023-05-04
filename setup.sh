3e9b19fed8e56a8f4e9aac29014bb38a5e0f49e3
arfprsty810 <94728173+arfprsty810@users.noreply.github.com>
2023-05-04 19:03:10 +0700

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -86,7 +86,81 @@ rm -fr $trgo
 rm -fr $logtrgo
 rm -fr /usr/bin/trojan-go
 clear
+
+echo ""
+date
+echo ""
+echo -e "[ ${green}INFO$NC ] INSTALLING REQUIREMENTS TOOLS"
+sleep 1
+
+cd /root/
+# // Remove
+apt-get remove --purge nginx* -y
+apt-get remove --purge apache2* -y
+apt autoremove -y
+clear
+
+cd /root/
+apt update && apt upgrade -y
+clear
+apt clean all && apt update
+clear
 apt install jq curl -y
+clear
+apt install iptables iptables-persistent -y
+clear
+apt install cron bash-completion -y
+clear
+apt install pwgen openssl netcat -y
+clear
+apt install lsb-release -y 
+clear
+apt install zip -y
+clear
+apt install net-tools -y
+clear
+apt install -y openvpn
+clear
+apt install socat xz-utils wget apt-transport-https gnupg gnupg2 gnupg1 dnsutils -y
+clear
+apt-get --reinstall --fix-missing install -y sudo dpkg psmisc ruby wondershaper python2 tmux nmap bzip2 gzip coreutils iftop htop unzip vim nano gcc g++ perl m4 dos2unix libreadline-dev zlib1g-dev git 
+clear
+apt-get --reinstall --fix-missing install -y screen rsyslog sed bc dirmngr neofetch screenfetch lsof easy-rsa libsqlite3-dev
+gem install lolcat
+clear
+apt-get install --no-install-recommends build-essential autoconf libtool libssl-dev libpcre3-dev libev-dev asciidoc xmlto -y
+clear
+
+apt -y install php php-fpm php-cli php-mysql libxml-parser-perl
+
+apt install make cmake automake -y
+apt install libz-dev -y
+apt install libssl1.0-dev -y
+apt install dos2unix -y
+clear
+
+apt-get install software-properties-common -y
+clear
+if [[ $OS == 'ubuntu' ]]; then
+apt install shadowsocks-libev -y
+apt install simple-obfs -y
+clear
+elif [[ $OS == 'debian' ]]; then
+if [[ "$ver" = "9" ]]; then
+echo "deb http://deb.debian.org/debian stretch-backports main" | tee /etc/apt/sources.list.d/stretch-backports.list
+apt update
+apt -t stretch-backports install shadowsocks-libev -y
+apt -t stretch-backports install simple-obfs -y
+clear
+elif [[ "$ver" = "10" ]]; then
+echo "deb http://deb.debian.org/debian buster-backports main" | tee /etc/apt/sources.list.d/buster-backports.list
+apt update
+apt -t buster-backports install shadowsocks-libev -y
+apt -t buster-backports install simple-obfs -y
+clear
+fi
+fi
+
 secs_to_human() {
     echo "Installation time : $(( ${1} / 3600 )) hours $(( (${1} / 60) % 60 )) minute's $(( ${1} % 60 )) seconds"
 }
@@ -134,17 +208,184 @@ read -rp "Input ur domain / sub-domain : " -e pp
     fi
 clear
 
-#Instal Xray
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "$green          INSTALLING SCRIPT $NC"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "[ ${green}SCRIPT${NC} ] install .... "
 sleep 2
+
+#Instal Xray
+wget $github/xray/ins-xray.sh && chmod +x ins-xray.sh && sed -i -e 's/\r$//' ins-xray.sh && ./ins-xray.sh
 clear
-wget $github/xray/ins-xray.sh && chmod +x ins-xray.sh && ./ins-xray.sh
-wget $github/ssh/ssh-vpn.sh && chmod +x ssh-vpn.sh && ./ssh-vpn.sh
-wget -q -O /usr/bin/bbr $github/bbr/bbr.sh && chmod +x /usr/bin/bbr && sed -i -e 's/\r$//' /usr/bin/bbr && screen -S bbr /bin/bbr
+sleep 2
+
+#Instal Trojan-GO
+wget $github/xray/trojan/trojan-go.sh && chmod +x trojan-go.sh && sed -i -e 's/\r$//' trojan-go.sh && ./trojan-go.sh
 clear
+sleep 2
+
+#Instal Shadowsocks
+wget $github/shadowsocks/shadowsocks.sh && chmod +x shadowsocks.sh sed -i -e 's/\r$//' shadowsocks.sh && ./shadowsocks.sh
+clear
+sleep 2
+
+#Instal SSH-vpn
+wget $github/ssh/ssh-vpn.sh && chmod +x ssh-vpn.sh && sed -i -e 's/\r$//' ssh-vpn.sh && ./ssh-vpn.sh
+clear
+sleep 2
+
+#Instal Bbr
+wget $github/bbr/bbr.sh && chmod +x bbr.sh && sed -i -e 's/\r$//' bbr.sh && screen -S bbr ./bbr
+clear
+sleep 2
+
+wget -q -O /usr/bin/restart "$github/services/restart.sh" && chmod +x /usr/bin/restart
+wget -q -O /usr/bin/running "$github/services/running.sh" && chmod +x /usr/bin/running
+wget -q -O /usr/bin/cek-bandwidth "$github/services/cek-bandwidth.sh" && chmod +x /usr/bin/cek-bandwidth
+wget -q -O /usr/bin/menu "$github/services/menu.sh" && chmod +x /usr/bin/menu
+wget -q -O /usr/bin/speedtest "$github/services/speedtest_cli.py" && chmod +x /usr/bin/speedtest
+wget -q -O /usr/bin/update "$github/services/update.sh" && chmod +x /usr/bin/update
+wget -q -O /usr/bin/renew-config "$github/backup/renew-config.sh" && chmod +x /usr/bin/renew-config
+wget -q -O /usr/bin/backup-user "$github/backup/backup-user.sh" && chmod +x /usr/bin/backup-user
+wget -q -O /usr/bin/cf "$github/services/cf.sh" && chmod +x /usr/bin/cf
+sed -i -e 's/\r$//' /bin/menu
+sed -i -e 's/\r$//' /bin/cek-bandwidth
+sed -i -e 's/\r$//' /bin/update
+sed -i -e 's/\r$//' /bin/restart
+sed -i -e 's/\r$//' /bin/running
+sed -i -e 's/\r$//' /bin/renew-config
+sed -i -e 's/\r$//' /bin/backup-user
+sed -i -e 's/\r$//' /bin/cf
+clear
+
+# ----------------------------------------------------------------------------------------------------------------
+# Restart Service
+# ----------------------------------------------------------------------------------------------------------------
+sleep 1
+echo -e "[ ${green}INFO$NC ] Restart All Service ..."
+systemctl daemon-reload >/dev/null 2>&1
+sleep 1
+echo -e "[ ${GREEN}ok${NC} ] Daemon-Reload"
+systemctl stop rc-local.service >/dev/null 2>&1
+systemctl restart rc-local.service >/dev/null 2>&1
+systemctl enable rc-local.service >/dev/null 2>&1
+systemctl start rc-local.service >/dev/null 2>&1
+sleep 1
+echo -e "[ ${GREEN}ok${NC} ] Restarting RC.Local"
+systemctl stop runn >/dev/null 2>&1
+systemctl restart runn >/dev/null 2>&1
+systemctl enable runn >/dev/null 2>&1
+systemctl start runn >/dev/null 2>&1
+sleep 1
+echo -e "[ ${GREEN}ok${NC} ] Restarting Run"
+/etc/init.d/nginx stop >/dev/null 2>&1
+/etc/init.d/nginx restart >/dev/null 2>&1
+/etc/init.d/nginx enable >/dev/null 2>&1
+/etc/init.d/nginx start >/dev/null 2>&1
+sleep 1
+echo -e "[ ${GREEN}ok${NC} ] Restarting Nginx "
+/etc/init.d/openvpn stop >/dev/null 2>&1
+/etc/init.d/openvpn restart >/dev/null 2>&1
+/etc/init.d/openvpn enable >/dev/null 2>&1
+/etc/init.d/openvpn start >/dev/null 2>&1
+sleep 1
+echo -e "[ ${GREEN}ok${NC} ] Restarting OpenVpn"
+/etc/init.d/dropbear stop >/dev/null 2>&1
+/etc/init.d/dropbear restart >/dev/null 2>&1
+/etc/init.d/dropbear enable >/dev/null 2>&1
+/etc/init.d/dropbear start >/dev/null 2>&1
+sleep 1
+echo -e "[ ${GREEN}ok${NC} ] Restarting Dropbear"
+/etc/init.d/stunnel5 stop >/dev/null 2>&1
+/etc/init.d/stunnel5 restart >/dev/null 2>&1
+/etc/init.d/stunnel5 enable >/dev/null 2>&1
+/etc/init.d/stunnel5 start >/dev/null 2>&1
+sleep 1
+echo -e "[ ${GREEN}ok${NC} ] Restarting Stunnel5"
+/etc/init.d/squid stop >/dev/null 2>&1
+/etc/init.d/squid restart >/dev/null 2>&1
+/etc/init.d/squid enable >/dev/null 2>&1
+/etc/init.d/squid start >/dev/null 2>&1
+sleep 1
+echo -e "[ ${GREEN}ok${NC} ] Restarting Squid "
+/etc/init.d/fail2ban stop >/dev/null 2>&1
+/etc/init.d/fail2ban restart >/dev/null 2>&1
+/etc/init.d/fail2ban enable >/dev/null 2>&1
+/etc/init.d/fail2ban start >/dev/null 2>&1
+sleep 1
+echo -e "[ ${GREEN}ok${NC} ] Restarting Fail2ban"
+/etc/init.d/cron stop >/dev/null 2>&1
+/etc/init.d/cron restart >/dev/null 2>&1
+/etc/init.d/cron enable >/dev/null 2>&1
+/etc/init.d/cron start >/dev/null 2>&1
+sleep 1
+echo -e "[ ${GREEN}ok${NC} ] Restarting Cron"
+/etc/init.d/vnstat stop >/dev/null 2>&1
+/etc/init.d/vnstat restart >/dev/null 2>&1
+/etc/init.d/vnstat enable >/dev/null 2>&1
+/etc/init.d/vnstat start >/dev/null 2>&1
+sleep 1
+echo -e "[ ${GREEN}ok${NC} ] Restarting Vnstat "
+/etc/init.d/ssh stop >/dev/null 2>&1
+/etc/init.d/ssh restart >/dev/null 2>&1
+/etc/init.d/ssh enable >/dev/null 2>&1
+/etc/init.d/ssh start >/dev/null 2>&1
+sleep 1
+echo -e "[ ${GREEN}ok${NC} ] Restarting SSH"
+systemctl stop xray >/dev/null 2>&1
+systemctl restart xray >/dev/null 2>&1
+systemctl enable xray >/dev/null 2>&1
+systemctl start xray >/dev/null 2>&1
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 2086 -j ACCEPT >/dev/null 2>&1
+iptables -I INPUT -m state --state NEW -m udp -p udp --dport 2087 -j ACCEPT >/dev/null 2>&1
+iptables-save > /etc/iptables.up.rules >/dev/null 2>&1
+iptables-restore -t < /etc/iptables.up.rules >/dev/null 2>&1
+netfilter-persistent save >/dev/null 2>&1
+netfilter-persistent reload >/dev/null 2>&1
+sleep 1
+echo -e "[ ${GREEN}ok${NC} ] Restarting Xray - VMESS / VLESS / TROJAN"
+systemctl stop trojan-go >/dev/null 2>&1
+systemctl restart trojan-go >/dev/null 2>&1
+systemctl enable trojan-go >/dev/null 2>&1
+systemctl start trojan-go >/dev/null 2>&1
+sleep 1
+echo -e "[ ${GREEN}ok${NC} ] Restarting Trojan-GO"
+/etc/init.d/shadowsocks-libev stop >/dev/null 2>&1
+/etc/init.d/shadowsocks-libev restart >/dev/null 2>&1
+/etc/init.d/shadowsocks-libev enable >/dev/null 2>&1
+/etc/init.d/shadowsocks-libev start >/dev/null 2>&1
+sleep 1
+echo -e "[ ${GREEN}ok${NC} ] Restarting ShadowSocks-OBFS"
+systemctl stop ws-stunnel.service
+systemctl restart ws-stunnel.service
+systemctl enable ws-stunnel.service
+systemctl start ws-stunnel.service
+sleep 1
+echo -e "[ ${GREEN}ok${NC} ] Restarting WS-Stunnel "
+#systemctl stop ws-dropbear.service >/dev/null 2>&1
+#systemctl restart ws-dropbear.service >/dev/null 2>&1
+#systemctl enable ws-dropbear.service >/dev/null 2>&1
+#systemctl start ws-dropbear.service >/dev/null 2>&1
+#sleep 1
+#echo -e "[ ${GREEN}ok${NC} ] Restarting WS-Dropbear"
+#/etc/init.d/sslh stop >/dev/null 2>&1
+#/etc/init.d/sslh restart >/dev/null 2>&1
+#/etc/init.d/sslh enable >/dev/null 2>&1
+#/etc/init.d/sslh start >/dev/null 2>&1
+#sleep 1
+#echo -e "[ ${GREEN}ok${NC} ] Restarting Sslh "
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 500 >/dev/null 2>&1
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 500 >/dev/null 2>&1
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 500 >/dev/null 2>&1
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7400 --max-clients 500 >/dev/null 2>&1
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7500 --max-clients 500 >/dev/null 2>&1
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7600 --max-clients 500 >/dev/null 2>&1
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7700 --max-clients 500 >/dev/null 2>&1
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7800 --max-clients 500 >/dev/null 2>&1
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7900 --max-clients 500 >/dev/null 2>&1
+sleep 1
+echo -e "[ ${GREEN}ok${NC} ] Restarting badvpn "
+echo ""
 
 cat> /root/.profile << END
 # ~/.profile: executed by Bourne-compatible login shells.

16ed39647510b509219a5bf8263df8d1324a4bf5
arfprsty810 <94728173+arfprsty810@users.noreply.github.com>
2023-05-05 14:28:09 +0700

    Add files via upload

--- xray/ins-xray.sh
+++ xray/ins-xray.sh
@@ -11,19 +11,6 @@ green() { echo -e "\\033[32;1m${*}\\033[0m"; }
 red() { echo -e "\\033[31;1m${*}\\033[0m"; }
 clear
 
-echo ""
-echo ""
-echo -e "[ ${green}INFO$NC ] INSTALLER AUTO SCRIPT "
-echo " - XRAY => VMESS - VLESS "
-echo " - TROJAN => TROJAN WS - TROJAN-GO "
-echo " - SHADOWSOCKS => SHADOWSOCKS-LIBEV "
-sleep 3
-echo -e ""
-clear
-
-echo -e "[ ${green}INFO$NC ] INSTALLING KONFIGURASI"
-sleep 1
-
 source /etc/os-release
 xray="/etc/xray"
 trgo="/etc/trojan-go"
@@ -32,9 +19,6 @@ ipvps="/var/lib/arf"
 github="https://raw.githubusercontent.com/arfprsty810/lite/main"
 OS=$ID
 ver=$VERSION_ID
-# set random pwd
-openssl rand -base64 16 > $xray/passwd
-pwd=$(cat $xray/passwd)
 # set random uuid
 uuid=$(cat /proc/sys/kernel/random/uuid)
 
@@ -43,99 +27,6 @@ echo ""
 domain=$(cat $xray/domain)
 sleep 1
 clear
-
-echo -e "[ ${green}INFO$NC ] INSTALLING REQRUITMENT"
-sleep 1
-cd /root/
-apt update && apt upgrade -y
-clear
-apt clean all && apt update
-clear
-apt install iptables iptables-persistent -y
-clear
-apt install cron bash-completion -y
-clear
-apt install pwgen openssl netcat -y
-clear
-apt install lsb-release -y 
-clear
-apt install zip -y
-clear
-apt install net-tools -y
-clear
-apt install socat xz-utils wget apt-transport-https gnupg gnupg2 gnupg1 dnsutils -y
-clear
-apt-get --reinstall --fix-missing install -y sudo dpkg psmisc ruby wondershaper python2 tmux nmap bzip2 gzip coreutils iftop htop unzip vim nano gcc g++ make perl m4 dos2unix libreadline-dev zlib1g-dev git 
-clear
-apt-get --reinstall --fix-missing install -y screen rsyslog sed bc dirmngr libxml-parser-perl neofetch screenfetch lsof easy-rsa fail2ban vnstat libsqlite3-dev #dropbear openvpn squid
-gem install lolcat
-clear
-apt-get install --no-install-recommends build-essential autoconf libtool libssl-dev libpcre3-dev libev-dev asciidoc xmlto automake -y
-clear
-
-apt-get install software-properties-common -y
-clear
-if [[ $OS == 'ubuntu' ]]; then
-apt install shadowsocks-libev -y
-apt install simple-obfs -y
-clear
-elif [[ $OS == 'debian' ]]; then
-if [[ "$ver" = "9" ]]; then
-echo "deb http://deb.debian.org/debian stretch-backports main" | tee /etc/apt/sources.list.d/stretch-backports.list
-apt update
-apt -t stretch-backports install shadowsocks-libev -y
-apt -t stretch-backports install simple-obfs -y
-clear
-elif [[ "$ver" = "10" ]]; then
-echo "deb http://deb.debian.org/debian buster-backports main" | tee /etc/apt/sources.list.d/buster-backports.list
-apt update
-apt -t buster-backports install shadowsocks-libev -y
-apt -t buster-backports install simple-obfs -y
-clear
-fi
-fi
-
-echo -e "[ ${green}INFO$NC ] DISABLE IPV6"
-sleep 1
-echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6 >/dev/null 2>&1
-sed -i '$ i\echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6' /etc/rc.local >/dev/null 2>&1
-apt update -y
-apt upgrade -y
-apt dist-upgrade -y
-clear
-echo -e "[ ${green}INFO${NC} ] CHECKING... "
-apt install iptables iptables-persistent -y
-sleep 1
-clear
-echo -e "[ ${green}INFO$NC ] SETTING NTPDATE"
-apt install ntpdate -y
-ntpdate -u pool.ntp.org
-ntpdate pool.ntp.org 
-timedatectl set-ntp true
-timedatectl set-timezone Asia/Jakarta
-sleep 1
-clear
-echo -e "[ ${green}INFO$NC ] ENABLE CHRONYD"
-apt -y install chrony
-systemctl enable chronyd
-systemctl restart chronyd
-sleep 1
-clear
-echo -e "[ ${green}INFO$NC ] ENABLE CHRONY"
-systemctl enable chrony
-systemctl restart chrony
-clear
-sleep 1
-clear
-echo -e "[ ${green}INFO$NC ] SETTING CHRONY TRACKING"
-chronyc sourcestats -v
-chronyc tracking -v
-clear
-echo -e "[ ${green}INFO$NC ] SETTING SERVICE"
-apt update -y
-apt upgrade -y
-clear
-echo " "
 
 # install xray
 echo -e "[ ${green}INFO$NC ] INSTALLING XRAY VMESS - VLESS"

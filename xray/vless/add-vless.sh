57f5f527dd4a624cf3ba10a2405c5bd6c5b0e356
arfprsty810 <94728173+arfprsty810@users.noreply.github.com>
2023-05-08 05:49:46 +0700

    Add files via upload

--- xray/vless/add-vless.sh
+++ xray/vless/add-vless.sh
@@ -11,12 +11,10 @@ green() { echo -e "\\033[32;1m${*}\\033[0m"; }
 red() { echo -e "\\033[31;1m${*}\\033[0m"; }
 
 clear
-arfvpn="/etc/arfvpn"
-xray="/etc/arfvpn/xray"
-ipvps="/var/lib/arfvpn"
+ipvps="/var/lib/arf"
 source $ipvps/ipvps.conf
 if [[ "$IP" = "" ]]; then
-domain=$(cat $xray/domain)
+domain=$(cat /etc/xray/domain)
 else
 domain=$IP
 fi
@@ -29,7 +27,7 @@ echo -e "\\E[0;41;36m      Add Xray/Vless Account      \E[0m"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 
 		read -rp "User: " -e user
-		CLIENT_EXISTS=$(grep -w $user $xray/config.json | wc -l)
+		CLIENT_EXISTS=$(grep -w $user /etc/xray/config.json | wc -l)
 
 		if [[ ${CLIENT_EXISTS} == '1' ]]; then
 clear
@@ -49,9 +47,9 @@ uuid=$(cat /proc/sys/kernel/random/uuid)
 read -p "Expired (days): " masaaktif
 exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
 sed -i '/#vless$/a\#vl# '"$user $exp"'\
-},{"id": "'""$uuid""'","email": "'""$user""'"' $xray/config.json
+},{"id": "'""$uuid""'","email": "'""$user""'"' /etc/xray/config.json
 sed -i '/#vlessgrpc$/a\#vl# '"$user $exp"'\
-},{"id": "'""$uuid""'","email": "'""$user""'"' $xray/config.json
+},{"id": "'""$uuid""'","email": "'""$user""'"' /etc/xray/config.json
 vlesslink1="vless://${uuid}@${domain}:$tls?path=/vless&security=tls&encryption=none&type=ws#${user}"
 vlesslink2="vless://${uuid}@${domain}:$none?path=/vless&encryption=none&type=ws#${user}"
 vlesslink3="vless://${uuid}@${domain}:$tls?mode=gun&security=tls&encryption=none&type=grpc&serviceName=vless-grpc&sni=bug.com#${user}"
--- xray/vless/del-vless.sh
+++ xray/vless/del-vless.sh
@@ -10,12 +10,8 @@ yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
 green() { echo -e "\\033[32;1m${*}\\033[0m"; }
 red() { echo -e "\\033[31;1m${*}\\033[0m"; }
 
-arfvpn="/etc/arfvpn"
-xray="/etc/arfvpn/xray"
-logxray="/var/log/arfvpn/xray"
-
 clear
-NUMBER_OF_CLIENTS=$(grep -c -E "^#vl# " "$xray/config.json")
+NUMBER_OF_CLIENTS=$(grep -c -E "^#vl# " "/etc/xray/config.json")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 		echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
         echo -e "\\E[0;41;36m       Delete Vless Account        \E[0m"
@@ -34,7 +30,7 @@ menu-vless
     echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
     echo "  User       Expired  " 
 	echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-	grep -E "^#vl# " "$xray/config.json" | cut -d ' ' -f 2-3 | column -t | sort | uniq
+	grep -E "^#vl# " "/etc/xray/config.json" | cut -d ' ' -f 2-3 | column -t | sort | uniq
     echo ""
     red "tap enter to go back"
     echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
@@ -42,8 +38,8 @@ menu-vless
     if [ -z $user ]; then
     menu-vless
     else
-    exp=$(grep -wE "^#vl# $user" "$xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
-    sed -i "/^#vl# $user $exp/,/^},{/d" $xray/config.json
+    exp=$(grep -wE "^#vl# $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
+    sed -i "/^#vl# $user $exp/,/^},{/d" /etc/xray/config.json
     systemctl restart xray > /dev/null 2>&1
     clear
     echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"

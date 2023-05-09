0475d04f8c58a921c0278fcb2e3b89e262d15793
arfprsty810 <94728173+arfprsty810@users.noreply.github.com>
2023-05-09 10:43:21 +0700

    Add files via upload

--- xray/ins-xray.sh
+++ xray/ins-xray.sh
@@ -56,13 +56,18 @@ clear
 
 echo -e "[ ${green}INFO$NC ] INSTALLING NGINX SERVER"
 # install webserver
-apt -y install nginx php php-fpm php-cli php-mysql libxml-parser-perl
+apt-get -y install nginx
+apt-get -y install php
+apt-get -y install php-fpm
+apt-get -y install php-cli
+apt-get -y install php-mysql
+apt-get -y install libxml-parser-perl
 rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
 wget -O /etc/nginx/nginx.conf "$github/xray/nginx.conf"
 wget -O /etc/nginx/conf.d/vps.conf "$github/xray/vps.conf"
 mkdir -p /home/vps/public_html
-sed -i 's/listen = \/var\/run\/php-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php/fpm/pool.d/www.conf
+sed -i 's/listen = \/var\/run\/php-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php/7.2/fpm/pool.d/www.conf
 useradd -m vps;
 echo "<?php phpinfo() ?>" > /home/vps/public_html/info.php
 chown -R www-data:www-data /home/vps/public_html

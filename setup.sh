935dd220efe10d9f3a6ca82d56d921007d50ceb8
arfprsty810 <94728173+arfprsty810@users.noreply.github.com>
2023-05-09 15:08:04 +0700

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -115,14 +115,14 @@ clear
 sleep 2
 
 # NGINX-SERVER
-wget $github/nginx/nginx-server.sh
-chmod +x nginx-server.sh
-sed -i -e 's/\r$//' nginx-server.sh
-./nginx-server.sh
+#wget https://raw.githubusercontent.com/arfprsty810/lite/main/nginx/nginx-server.sh
+#chmod +x nginx-server.sh
+#sed -i -e 's/\r$//' nginx-server.sh
+#./nginx-server.sh
 
 #SSL CERT
-wget -O /usr/bin/cert $github/cert/cert.sh && chmod +x /usr/bin/cert && sed -i -e 's/\r$//' /usr/bin/cert
-/usr/bin/cert
+#wget -O /usr/bin/cert $github/cert/cert.sh && chmod +x /usr/bin/cert && sed -i -e 's/\r$//' /usr/bin/cert
+#/usr/bin/cert
 
 #Instal Xray
 wget $github/xray/ins-xray.sh

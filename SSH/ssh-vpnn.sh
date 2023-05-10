9308c0e23940c6134e1fc4176c8d3900cdeb6742
arfprsty810 <94728173+arfprsty810@users.noreply.github.com>
2023-05-10 20:28:32 +0700

    Add files via upload

--- SSH/ssh-vpnn.sh
+++ SSH/ssh-vpnn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# ==========================================
+# ==================================================
 # Color
 RED='\033[0;31m'
 NC='\033[0m'
@@ -9,9 +9,8 @@ BLUE='\033[0;34m'
 PURPLE='\033[0;35m'
 CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
-# ==========================================
-
 clear
+
 # ==================================================
 # Link
 sshlink="raw.githubusercontent.com/arfprsty810/lite/main/SSH"
@@ -19,81 +18,22 @@ stunnel5link="raw.githubusercontent.com/arfprsty810/lite/main/stunnel5"
 websocketlink="raw.githubusercontent.com/arfprsty810/lite/main/SSH/websocket"
 
 # source /etc/os-release
+# ==================================================
 export DEBIAN_FRONTEND=noninteractive
+source /etc/os-release
+ver=$VERSION_ID
 arfvpn="/etc/arfvpn"
 ipvps="/var/lib/arfvpn"
 MYIP=$(cat $arfvpn/IP);
 DOMAIN=$(cat $arfvpn/domain)
-MYIP2="s/xxxxxxxxx/$MYIP/g";
+MYIP2="s/xxxxxxxxx/139.59.110.197/g";
 NET=$(ip -o $ANU -4 route show to default | awk '{print $5}');
-source /etc/os-release
-ver=$VERSION_ID
-
-# ----------------------------------------------------------------------------------------------------------------
-# Setting RC.Local
-# ----------------------------------------------------------------------------------------------------------------
-# nano /etc/rc.local
-cat > /etc/rc.local <<-END
-#!/bin/sh -e
-# rc.local
-# By default this script does nothing.
-exit 0
-END
-
-cat > /etc/systemd/system/rc-local.service <<-END
-[Unit]
-Description=/etc/rc.local
-ConditionPathExists=/etc/rc.local
-[Service]
-Type=forking
-ExecStart=/etc/rc.local start
-TimeoutSec=0
-StandardOutput=tty
-RemainAfterExit=yes
-SysVStartPriority=99
-[Install]
-WantedBy=multi-user.target
-END
-
-# Ubah izin akses
-chmod +x /etc/rc.local
-echo -e " "
-date
-echo ""
-# enable rc local
-sleep 1
-echo -e "[ ${GREEN}INFO${NC} ] Checking... "
-sleep 2
-sleep 1
-echo -e "[ ${GREEN}INFO$NC ] Enable system rc local"
-systemctl enable rc-local >/dev/null 2>&1
-systemctl start rc-local.service >/dev/null 2>&1
-
-# ----------------------------------------------------------------------------------------------------------------
-# Install BadVPN
-# ----------------------------------------------------------------------------------------------------------------
-cd
-cd
-wget -O /usr/bin/badvpn-udpgw "https://${sshlink}/badvpn-udpgw64"
-chmod +x /usr/bin/badvpn-udpgw
-sed -i '$ i\screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 500' /etc/rc.local
-sed -i '$ i\screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 500' /etc/rc.local
-sed -i '$ i\screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 500' /etc/rc.local
-screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 500
-screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 500
-screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 500
-screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7400 --max-clients 500
-screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7500 --max-clients 500
-screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7600 --max-clients 500
-screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7700 --max-clients 500
-screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7800 --max-clients 500
-screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7900 --max-clients 500
 
 # ----------------------------------------------------------------------------------------------------------------
 # Setting sshd_config
 # ----------------------------------------------------------------------------------------------------------------
 echo "Configuring SSH."
-sed -i 's/Port 22/Port 22/g' /etc/ssh/sshd_config
+sed -i 's/#Port 22/Port 22/g' /etc/ssh/sshd_config
 
 cd /etc/pam.d
 [ -f "common-password" ] || mv common-password common-pass-old
@@ -118,25 +58,99 @@ echo "/usr/sbin/nologin" >> /etc/shells
 # ----------------------------------------------------------------------------------------------------------------
 # Install Banner
 # ----------------------------------------------------------------------------------------------------------------
-wget -O /etc/issue.net "https://${sshlink}/issue.net"
+wget -O /etc/issue.net "https://raw.githubusercontent.com/arfprsty810/lite/main/SSH/issue.net"
 echo "Banner /etc/issue.net" >>/etc/ssh/sshd_config
 sed -i 's@DROPBEAR_BANNER=""@DROPBEAR_BANNER="/etc/issue.net"@g' /etc/default/dropbear
+
+# ----------------------------------------------------------------------------------------------------------------
+# Install BadVPN
+# ----------------------------------------------------------------------------------------------------------------
+cd
+tesmatch=`screen -list | awk  '{print $1}' | grep -ow "badvpn" | sort | uniq`
+if [ "$tesmatch" = "badvpn" ]; then
+sleep 1
+echo -e "[ ${GREEN}INFO$NC ] Screen badvpn detected"
+rm /root/screenlog > /dev/null 2>&1
+    runningscreen=( `screen -list | awk  '{print $1}' | grep -w "badvpn"` ) # sed 's/\.[^ ]*/ /g'
+    for actv in "${runningscreen[@]}"
+    do
+        cek=( `screen -list | awk  '{print $1}' | grep -w "badvpn"` )
+        if [ "$cek" = "$actv" ]; then
+        for sama in "${cek[@]}"; do
+            sleep 1
+            screen -XS $sama quit > /dev/null 2>&1
+            echo -e "[ ${red}CLOSE$NC ] Closing screen $sama"
+        done 
+        fi
+    done
+else
+echo -ne
+fi
+wget -O /usr/bin/badvpn-udpgw "https://raw.githubusercontent.com/arfprsty810/lite/main/SSH/badvpn-udpgw64"
+chmod +x /usr/bin/badvpn-udpgw
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7400 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7500 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7600 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7700 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7800 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7900 --max-clients 500
+
+# ----------------------------------------------------------------------------------------------------------------
+# Setting RC.Local
+# ----------------------------------------------------------------------------------------------------------------
+# nano /etc/rc.local
+cat > /etc/rc.local <<-END
+#!/bin/sh -e
+# rc.local
+# By default this script does nothing.
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7400 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7500 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7600 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7700 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7800 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7900 --max-clients 500
+exit 0
+END
+
+cat > /etc/systemd/system/rc-local.service <<-END
+[Unit]
+Description=/etc/rc.local
+ConditionPathExists=/etc/rc.local
+[Service]
+Type=forking
+ExecStart=/etc/rc.local start
+TimeoutSec=0
+StandardOutput=tty
+RemainAfterExit=yes
+SysVStartPriority=99
+[Install]
+WantedBy=multi-user.target
+END
+
+chmod +x /etc/rc.local
+systemctl enable rc-local >/dev/null 2>&1
+systemctl start rc-local.service >/dev/null 2>&1
 
 # ----------------------------------------------------------------------------------------------------------------
 # Install Squid
 # ----------------------------------------------------------------------------------------------------------------
 cd
 apt -y install squid3
-wget -O /etc/squid/squid.conf "https://${sshlink}/squid3.conf"
-sed -i $MYIP2 /etc/squid/squid.conf
+wget -O /etc/squid/squid.conf "https://raw.githubusercontent.com/arfprsty810/lite/main/SSH/squid3.conf"
+sed -i "s/xxxxxxxxx/139.59.110.197/g" /etc/squid/squid.conf
 
 # ----------------------------------------------------------------------------------------------------------------
 # Install SSLH
 # ----------------------------------------------------------------------------------------------------------------
 apt -y install sslh
-rm -f /etc/default/sslh
 
-# Settings SSLH
+rm -f /etc/default/sslh
 cat > /etc/default/sslh <<-END
 # Default options for sslh initscript
 # sourced by /etc/init.d/sslh
@@ -169,10 +183,11 @@ systemctl restart sslh
 # ----------------------------------------------------------------------------------------------------------------
 # Install VNSTAT
 # ----------------------------------------------------------------------------------------------------------------
-apt -y install vnstat
-/etc/init.d/vnstat restart
-apt -y install libsqlite3-dev
-wget https://${sshlink}/vnstat-2.6.tar.gz
+cd
+#apt -y install vnstat
+#/etc/init.d/vnstat restart
+#apt -y install libsqlite3-dev
+wget https://raw.githubusercontent.com/arfprsty810/lite/main/SSH/vnstat-2.6.tar.gz
 tar zxvf vnstat-2.6.tar.gz
 cd vnstat-2.6
 ./configure --prefix=/usr --sysconfdir=/etc && make && make install
@@ -189,7 +204,7 @@ rm -rf /root/vnstat-2.6
 # Install Stunnel5
 # ----------------------------------------------------------------------------------------------------------------
 cd /root/
-wget -q -O stunnel5.zip "https://${stunnel5link}/stunnel5.zip"
+wget -q -O stunnel5.zip "https://raw.githubusercontent.com/arfprsty810/lite/main/stunnel5/stunnel5.zip"
 unzip -o stunnel5.zip
 cd /root/stunnel
 chmod +x configure
@@ -202,7 +217,6 @@ rm -f stunnel5.zip
 mkdir -p /etc/stunnel5
 chmod 644 /etc/stunnel5
 
-mkdir -p /etc/arfvpn
 wget -O /etc/arfvpn/stunnel.pem "https://raw.githubusercontent.com/arfprsty810/lite/main/cert/client.pem"
 chmod 600 /etc/arfvpn/stunnel.pem
 
@@ -244,7 +258,7 @@ WantedBy=multi-user.target
 END
 
 # Service Stunnel5 /etc/init.d/stunnel5
-wget -q -O /etc/init.d/stunnel5 "https://${stunnel5link}/stunnel5.init"
+wget -q -O /etc/init.d/stunnel5 "https://raw.githubusercontent.com/arfprsty810/lite/main/stunnel5/stunnel5.init"
 
 # Ubah Izin Akses
 chmod 600 /etc/stunnel5/stunnel5.pem
@@ -269,18 +283,14 @@ systemctl restart stunnel5
 /etc/init.d/stunnel5 restart
 
 # ----------------------------------------------------------------------------------------------------------------
-# Install OpemVPN
+# Install OpenVPN
 # ----------------------------------------------------------------------------------------------------------------
-wget https://${sshlink}/vpn.sh &&  chmod +x vpn.sh && ./vpn.sh
-
-# ----------------------------------------------------------------------------------------------------------------
-# Install Fail2Ban
-# ----------------------------------------------------------------------------------------------------------------
-apt -y install fail2ban
+wget https://raw.githubusercontent.com/arfprsty810/lite/main/SSH/vpn.sh &&  chmod +x vpn.sh && ./vpn.sh
 
 # ----------------------------------------------------------------------------------------------------------------
 # Install Ddos
 # ----------------------------------------------------------------------------------------------------------------
+rm -fr /usr/local/ddos
 if [ -d '/usr/local/ddos' ]; then
 	echo; echo; echo "Please un-install the previous version first"
 	exit 0
@@ -310,7 +320,7 @@ echo 'Please send in your comments and/or suggestions to zaf@vsnl.com'
 # ----------------------------------------------------------------------------------------------------------------
 # Install BBR
 # ----------------------------------------------------------------------------------------------------------------
-wget https://${sshlink}/bbr.sh && chmod +x bbr.sh && ./bbr.sh
+wget https://raw.githubusercontent.com/arfprsty810/lite/main/SSH/bbr.sh && chmod +x bbr.sh && ./bbr.sh
 
 # ----------------------------------------------------------------------------------------------------------------
 # Install Blockir Torrent
@@ -333,48 +343,48 @@ netfilter-persistent reload
 # ----------------------------------------------------------------------------------------------------------------
 # Install WebSocket
 # ----------------------------------------------------------------------------------------------------------------
-cd
-wget "https://${websocketlink}/edu.sh"
-chmod +x edu.sh
-./edu.sh
+#cd
+#wget "https://raw.githubusercontent.com/arfprsty810/lite/main/SSH/websocket/edu.sh"
+#chmod +x edu.sh
+#./edu.sh
 
 # ----------------------------------------------------------------------------------------------------------------
 # Download Script
 # ----------------------------------------------------------------------------------------------------------------
 cd /usr/bin
-wget -O addhost "https://${sshlink}/addhost.sh"
-wget -O about "https://${sshlink}/about.sh"
-wget -O menuvpn "https://${sshlink}/menu.sh"
-wget -O addssh "https://${sshlink}/addssh.sh"
-wget -O trialssh "https://${sshlink}/trialssh.sh"
-wget -O delssh "https://${sshlink}/delssh.sh"
-wget -O member "https://${sshlink}/member.sh"
-wget -O delexp "https://${sshlink}/delexp.sh"
-wget -O cekssh "https://${sshlink}/cekssh.sh"
-wget -O restart "https://${sshlink}/restart.sh"
-wget -O speedtest "https://${sshlink}/speedtest_cli.py"
-wget -O info "https://${sshlink}/info.sh"
-wget -O ram "https://${sshlink}/ram.sh"
-wget -O renewssh "https://${sshlink}/renewssh.sh"
-wget -O autokill "https://${sshlink}/autokill.sh"
-wget -O ceklim "https://${sshlink}/ceklim.sh"
-wget -O tendang "https://${sshlink}/tendang.sh"
-wget -O clearlog "https://${sshlink}/clearlog.sh"
-wget -O changeport "https://${sshlink}/changeport.sh"
-wget -O portovpn "https://${sshlink}/portovpn.sh"
-wget -O portwg "https://${sshlink}/portwg.sh"
-wget -O porttrojan "https://${sshlink}/porttrojan.sh"
-wget -O portsstp "https://${sshlink}/portsstp.sh"
-wget -O portsquid "https://${sshlink}/portsquid.sh"
-wget -O portvlm "https://${sshlink}/portvlm.sh"
-wget -O wbmn "https://${sshlink}/webmin.sh"
-wget -O xp "https://${sshlink}/xp.sh"
-wget -O swapkvm "https://${sshlink}/swapkvm.sh"
-wget -O certsslh "https://${sshlink}/certsslh.sh"
-wget -O cfnhost "https://${sshlink}/cfnhost.sh"
+wget -O addhost "https://raw.githubusercontent.com/arfprsty810/lite/main/SSH/addhost.sh"
+wget -O about "https://raw.githubusercontent.com/arfprsty810/lite/main/SSH/about.sh"
+wget -O menuvpn "https://raw.githubusercontent.com/arfprsty810/lite/main/SSH/menu.sh"
+wget -O addssh "https://raw.githubusercontent.com/arfprsty810/lite/main/SSH/addssh.sh"
+wget -O trialssh "https://raw.githubusercontent.com/arfprsty810/lite/main/SSH/trialssh.sh"
+wget -O delssh "https://raw.githubusercontent.com/arfprsty810/lite/main/SSH/delssh.sh"
+wget -O member "https://raw.githubusercontent.com/arfprsty810/lite/main/SSH/member.sh"
+wget -O delexp "https://raw.githubusercontent.com/arfprsty810/lite/main/SSH/delexp.sh"
+wget -O cekssh "https://raw.githubusercontent.com/arfprsty810/lite/main/SSH/cekssh.sh"
+wget -O restart "https://raw.githubusercontent.com/arfprsty810/lite/main/SSH/restart.sh"
+wget -O speedtest "https://raw.githubusercontent.com/arfprsty810/lite/main/SSH/speedtest_cli.py"
+wget -O info "https://raw.githubusercontent.com/arfprsty810/lite/main/SSH/info.sh"
+wget -O ram "https://raw.githubusercontent.com/arfprsty810/lite/main/SSH/ram.sh"
+wget -O renewssh "https://raw.githubusercontent.com/arfprsty810/lite/main/SSH/renewssh.sh"
+wget -O autokill "https://raw.githubusercontent.com/arfprsty810/lite/main/SSH/autokill.sh"
+wget -O ceklim "https://raw.githubusercontent.com/arfprsty810/lite/main/SSH/ceklim.sh"
+wget -O tendang "https://raw.githubusercontent.com/arfprsty810/lite/main/SSH/tendang.sh"
+wget -O clearlog "https://raw.githubusercontent.com/arfprsty810/lite/main/SSH/clearlog.sh"
+wget -O changeport "https://raw.githubusercontent.com/arfprsty810/lite/main/SSH/changeport.sh"
+wget -O portovpn "https://raw.githubusercontent.com/arfprsty810/lite/main/SSH/portovpn.sh"
+wget -O portwg "https://raw.githubusercontent.com/arfprsty810/lite/main/SSH/portwg.sh"
+wget -O porttrojan "https://raw.githubusercontent.com/arfprsty810/lite/main/SSH/porttrojan.sh"
+wget -O portsstp "https://raw.githubusercontent.com/arfprsty810/lite/main/SSH/portsstp.sh"
+wget -O portsquid "https://raw.githubusercontent.com/arfprsty810/lite/main/SSH/portsquid.sh"
+wget -O portvlm "https://raw.githubusercontent.com/arfprsty810/lite/main/SSH/portvlm.sh"
+wget -O wbmn "https://raw.githubusercontent.com/arfprsty810/lite/main/SSH/webmin.sh"
+wget -O xp "https://raw.githubusercontent.com/arfprsty810/lite/main/SSH/xp.sh"
+wget -O swapkvm "https://raw.githubusercontent.com/arfprsty810/lite/main/SSH/swapkvm.sh"
+wget -O certsslh "https://raw.githubusercontent.com/arfprsty810/lite/main/SSH/certsslh.sh"
+wget -O cfnhost "https://raw.githubusercontent.com/arfprsty810/lite/main/SSH/cfnhost.sh"
 
-wget -O portsshws "https://${websocketlink}/portsshws.sh"
-wget -O portsshnontls "https://${websocketlink}/portsshnontls.sh"
+wget -O portsshws "https://raw.githubusercontent.com/arfprsty810/lite/main/SSH/websocket/portsshws.sh"
+wget -O portsshnontls "https://raw.githubusercontent.com/arfprsty810/lite/main/SSH/websocket/portsshnontls.sh"
 
 chmod +x addhost
 chmod +x menuvpn
@@ -451,13 +461,4 @@ screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7600 --max-clients 500
 screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7700 --max-clients 500
 screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7800 --max-clients 500
 screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7900 --max-clients 500
-history -c
-echo "unset HISTFILE" >> /etc/profile
-
-cd
-rm -f /root/key.pem
-rm -f /root/cert.pem
-rm -f /root/ssh-vpn.sh
-
-# finihsing
 clear

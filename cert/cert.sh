8f430303e0615d50db5e576d2b4df9649ee7f28c
arfprsty810 <94728173+arfprsty810@users.noreply.github.com>
2023-05-11 18:02:49 +0700

    Add files via upload

--- cert/cert.sh
+++ cert/cert.sh
@@ -37,8 +37,8 @@ clear
 ## crt ssl cloudflare sg.d-jumper.me *.sg.d-jumper.me
 wget -O $arfvpn/arfvpn.crt "$github/cert/arfvpn.crt"
 wget -O $arfvpn/arfvpn.key "$github/cert/arfvpn.key"
-wget -O $arfvpn/nginx.crt "$github/cert/nginx.crt"
-wget -O $arfvpn/nginx.key "$github/cert/nginx.key"
+wget -O $arfvpn/nginx/nginx.crt "$github/cert/nginx.crt"
+wget -O $arfvpn/nginx/nginx.key "$github/cert/nginx.key"
 
 echo -e "[ ${green}INFO$NC ] RENEW CERT SSL"
 # nginx renew ssl
@@ -46,8 +46,8 @@ echo -n '#!/bin/bash
 /etc/init.d/nginx stop
 wget -O $arfvpn/arfvpn.crt "$github/cert/arfvpn.crt"
 wget -O $arfvpn/arfvpn.key "$github/cert/arfvpn.key"
-wget -O $arfvpn/nginx.crt "$github/cert/nginx.crt"
-wget -O $arfvpn/nginx.key "$github/cert/nginx.key"
+wget -O $arfvpn/nginx/nginx.crt "$github/cert/nginx.crt"
+wget -O $arfvpn/nginx/nginx.key "$github/cert/nginx.key"
 /etc/init.d/nginx start
 ' > /usr/local/bin/ssl_renew.sh
 chmod +x /usr/local/bin/ssl_renew.sh

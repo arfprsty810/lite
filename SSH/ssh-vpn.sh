a6de5e53eda893233e2d860416f7f54aa63d98c8
arfprsty810 <94728173+arfprsty810@users.noreply.github.com>
2023-05-12 14:36:39 +0700

    Add files via upload

--- SSH/ssh-vpn.sh
+++ SSH/ssh-vpn.sh
@@ -11,25 +11,32 @@ CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 # ==========================================
 
-clear
 # ==================================================
-# Link Hosting Kalian
+# Link
 sshlink="raw.githubusercontent.com/arfprsty810/lite/main/SSH"
-
-# Link Hosting Kalian Untuk Stunnel5
-stunnel5link="raw.githubusercontent.com/arfprsty810/lite/main/SSH"
-
-#Link Hosting Kalian Untuk Websocket
+stunnel5link="raw.githubusercontent.com/arfprsty810/lite/main/stunnel5"
 websocketlink="raw.githubusercontent.com/arfprsty810/lite/main/SSH/websocket"
 
-# initializing var
+# source /etc/os-release
+# ==================================================
 export DEBIAN_FRONTEND=noninteractive
-arfvpn="/etc/arfvpn"
-MYIP=$(cat $arfvpn/IP);
-MYIP2="s/xxxxxxxxx/$MYIP/g";
-NET=$(ip -o $ANU -4 route show to default | awk '{print $5}');
 source /etc/os-release
 ver=$VERSION_ID
+arfvpn="/etc/arfvpn"
+ipvps="/var/lib/arfvpn"
+MYIP=$(cat $arfvpn/IP);
+DOMAIN=$(cat $arfvpn/domain)
+MYIP2="s/xxxxxxxxx/139.59.110.197/g";
+NET=$(ip -o $ANU -4 route show to default | awk '{print $5}');
+
+#detail nama perusahaan
+country=ID
+state=Indonesia
+locality=Indonesia
+organization=gl33ch3rvpn
+organizationalunit=gl33ch3rvpn
+commonname=gl33ch3rvpn
+email=akbarssh21@gmail.com
 
 # simple password minimal
 wget -O /etc/pam.d/common-password "https://${sshlink}/password"
@@ -69,6 +76,26 @@ chmod +x /etc/rc.local
 systemctl enable rc-local
 systemctl start rc-local.service
 
+# set locale
+sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
+
+# install webserver
+apt -y install nginx php php-fpm php-cli php-mysql libxml-parser-perl
+rm /etc/nginx/sites-enabled/default
+rm /etc/nginx/sites-available/default
+curl https://${sshlink}/nginx.conf > /etc/nginx/nginx.conf
+curl https://${sshlink}/vps.conf > /etc/nginx/conf.d/vps.conf
+sed -i 's/listen = \/var\/run\/php-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php/fpm/pool.d/www.conf
+useradd -m vps;
+mkdir -p /home/vps/public_html
+echo "<?php phpinfo() ?>" > /home/vps/public_html/info.php
+chown -R www-data:www-data /home/vps/public_html
+chmod -R g+rw /home/vps/public_html
+cd /home/vps/public_html
+wget -O /home/vps/public_html/index.html "https://${sshlink}/index.html1"
+/etc/init.d/nginx restart
+cd
+
 # install badvpn
 cd
 wget -O /usr/bin/badvpn-udpgw "https://${sshlink}/badvpn-udpgw64"
@@ -99,17 +126,17 @@ echo "/usr/sbin/nologin" >> /etc/shells
 /etc/init.d/dropbear restart
 
 # install squid
-#cd
-#apt -y install squid3
-#wget -O /etc/squid/squid.conf "https://${sshlink}/squid3.conf"
-#sed -i $MYIP2 /etc/squid/squid.conf
+cd
+apt -y install squid3
+wget -O /etc/squid/squid.conf "https://${sshlink}/squid3.conf"
+sed -i $MYIP2 /etc/squid/squid.conf
 
 # Install SSLH
-#apt -y install sslh
-#rm -f /etc/default/sslh
+apt -y install sslh
+rm -f /etc/default/sslh
 
 # Settings SSLH
-#cat > /etc/default/sslh <<-END
+cat > /etc/default/sslh <<-END
 # Default options for sslh initscript
 # sourced by /etc/init.d/sslh
 
@@ -121,22 +148,22 @@ echo "/usr/sbin/nologin" >> /etc/shells
 # Once configuration ready, you *must* set RUN to yes here
 # and try to start sslh (standalone mode only)
 
-#RUN=yes
+RUN=yes
 
 # binary to use: forked (sslh) or single-thread (sslh-select) version
 # systemd users: don't forget to modify /lib/systemd/system/sslh.service
-#DAEMON=/usr/sbin/sslh
+DAEMON=/usr/sbin/sslh
 
-#DAEMON_OPTS="--user sslh --listen 0.0.0.0:443 --ssl 127.0.0.1:777 --ssh 127.0.0.1:109 --openvpn 127.0.0.1:1194 --http 127.0.0.1:8880 --pidfile /var/run/sslh/sslh.pid -n"
+DAEMON_OPTS="--user sslh --listen 0.0.0.0:443 --ssl 127.0.0.1:777 --ssh 127.0.0.1:109 --openvpn 127.0.0.1:1194 --http 127.0.0.1:8880 --pidfile /var/run/sslh/sslh.pid -n"
 
-#END
+END
 
 # Restart Service SSLH
-#service sslh restart
-#systemctl restart sslh
-#/etc/init.d/sslh restart
-#/etc/init.d/sslh status
-#/etc/init.d/sslh restart
+service sslh restart
+systemctl restart sslh
+/etc/init.d/sslh restart
+/etc/init.d/sslh status
+/etc/init.d/sslh restart
 
 # setting vnstat
 apt -y install vnstat
@@ -170,12 +197,10 @@ rm -f stunnel5.zip
 mkdir -p /etc/stunnel5
 chmod 644 /etc/stunnel5
 
-wget -O /etc/arfvpn/stunnel.pem "https://raw.githubusercontent.com/arfprsty810/lite/main/cert/client.pem"
-chmod 600 /etc/arfvpn/stunnel.pem
-
 # Download Config Stunnel5
 cat > /etc/stunnel5/stunnel5.conf <<-END
-cert = /etc/arfvpn/stunnel.pem
+cert = /etc/xray/xray.crt
+key = /etc/xray/xray.key
 client = no
 socket = a:SO_REUSEADDR=1
 socket = l:TCP_NODELAY=1
@@ -303,21 +328,14 @@ netfilter-persistent reload
 
 # download script
 cd /usr/bin
-wget -q -O /usr/bin/menu-ssh "https://raw.githubusercontent.com/arfprsty810/lite/main/ssh/menu-ssh.sh"
-chmod +x /usr/bin/menu-ssh
-sed -i -e 's/\r$//' /usr/bin/menu-ssh
 wget -O addhost "https://${sshlink}/addhost.sh"
-wget -O about "https://${sshlink}/about.sh"
-wget -O menussh "https://${sshlink}/menu.sh"
+wget -O menuvpn "https://${sshlink}/menu.sh"
 wget -O addssh "https://${sshlink}/addssh.sh"
 wget -O trialssh "https://${sshlink}/trialssh.sh"
 wget -O delssh "https://${sshlink}/delssh.sh"
 wget -O member "https://${sshlink}/member.sh"
 wget -O delexp "https://${sshlink}/delexp.sh"
 wget -O cekssh "https://${sshlink}/cekssh.sh"
-wget -O restart "https://${sshlink}/restart.sh"
-wget -O speedtest "https://${sshlink}/speedtest_cli.py"
-wget -O info "https://${sshlink}/info.sh"
 wget -O ram "https://${sshlink}/ram.sh"
 wget -O renewssh "https://${sshlink}/renewssh.sh"
 wget -O autokill "https://${sshlink}/autokill.sh"
@@ -336,19 +354,17 @@ wget -O xp "https://${sshlink}/xp.sh"
 wget -O swapkvm "https://${sshlink}/swapkvm.sh"
 wget -O certsslh "https://${sshlink}/certsslh.sh"
 wget -O cfnhost "https://${sshlink}/cfnhost.sh"
+wget -O portsshws "https://${websocketlink}/portsshws.sh"
+wget -O portsshnontls "https://${websocketlink}/portsshnontls.sh"
 
 chmod +x addhost
-chmod +x menussh
+chmod +x menuvpn
 chmod +x addssh
 chmod +x trialssh
 chmod +x delssh
 chmod +x member
 chmod +x delexp
 chmod +x cekssh
-chmod +x restart
-chmod +x speedtest
-chmod +x info
-chmod +x about
 chmod +x autokill
 chmod +x tendang
 chmod +x ceklim
@@ -365,6 +381,48 @@ chmod +x portvlm
 chmod +x wbmn
 chmod +x xp
 chmod +x swapkvm
+chmod +x cfnhost
+chmod +x certsslh
+
+# remove unnecessary files
+cd
+apt autoclean -y
+apt -y remove --purge unscd
+apt-get -y --purge remove samba*;
+apt-get -y --purge remove apache2*;
+apt-get -y --purge remove bind9*;
+apt-get -y remove sendmail*
+apt autoremove -y
+# finishing
+cd
+chown -R www-data:www-data /home/vps/public_html
+/etc/init.d/nginx restart
+/etc/init.d/openvpn restart
+/etc/init.d/cron restart
+/etc/init.d/ssh restart
+/etc/init.d/dropbear restart
+/etc/init.d/fail2ban restart
+/etc/init.d/sslh restart
+/etc/init.d/stunnel5 restart
+/etc/init.d/vnstat restart
+/etc/init.d/fail2ban restart
+/etc/init.d/squid restart
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7400 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7500 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7600 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7700 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7800 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7900 --max-clients 500
+history -c
+echo "unset HISTFILE" >> /etc/profile
+
+cd
+rm -f /root/key.pem
+rm -f /root/cert.pem
+rm -f /root/ssh-vpn.sh
 
 # finihsing
 clear

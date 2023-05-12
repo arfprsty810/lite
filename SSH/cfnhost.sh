aafd5ea7ecd9b4cc18011baf98439a9d98938eee
arfprsty810 <94728173+arfprsty810@users.noreply.github.com>
2023-05-12 14:42:18 +0700

    Add files via upload

---
+++ SSH/cfnhost.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# My Telegram : https://t.me/Akbar218
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+
+error1="${RED}[ERROR]${NC}"
+success="${GREEN}[SUCCESS]${NC}"
+
+# Getting
+arfvpn="/etc/arfvpn"
+MYIP=$(cat $arfvpn/IP);
+
+clear
+
+echo -e "========================="
+read -rp "Input Domain/Host : " -e domain
+echo -e "========================="
+echo -e "${success}\nDomain : ${domain} Added.."
+# Delete Files
+rm /var/lib/gl33ch3rvpn/cfndomain
+# Done
+echo "${domain}" >> /var/lib/gl33ch3rvpn/cfndomain
+sleep 5

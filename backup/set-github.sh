c9976ebac133c2675848e7b579d6e81ddb57464a
arfprsty810 <94728173+arfprsty810@users.noreply.github.com>
2023-05-13 11:43:03 +0700

    Add files via upload

--- backup/set-github.sh
+++ backup/set-github.sh
@@ -3,8 +3,6 @@ apt install git -y
 clear
 # https://github.com/arfprsty810/backup.git
 cd
-mkdir -p /root/github
-echo " ex. : https://github.com/your_github/backup.git
 read -rp " Input your github username : " -e username
 #echo "$username" > /root/github/username
 read -rp " Input your github project : " -e project
@@ -13,7 +11,7 @@ echo "https://github.com/$username/$project.git" > /root/github.git
 github=$(cat /root/github.git)
 clear
 
-cd /root
+cd
 rm -rvf /root/backup
 mkdir -p /root/backup
 #cp /etc/arfvpn/* /root/backup/

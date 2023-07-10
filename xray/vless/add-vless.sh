c8a946bda303846f60a3fa590cb349719ffa9b3c
arfprsty810 <94728173+arfprsty810@users.noreply.github.com>
2023-07-10 16:44:40 +0700

    Update add-vless.sh

--- xray/vless/add-vless.sh
+++ xray/vless/add-vless.sh
@@ -14,22 +14,22 @@ clear
 arfvpn="/etc/arfvpn"
 xray="/etc/xray"
 ipvps="/var/lib/arfvpn"
-source $ipvps/ipvps.conf
-if [[ "$IP" = "" ]]; then
-domain=$(cat $xray/domain)
+source ${ipvps}/ipvps.conf
+if [[ "${IP}" = "" ]]; then
+domain=$(cat ${xray}/domain)
 else
-domain=$IP
+domain=${IP}
 fi
 
 tls="$(cat ~/log-install.txt | grep -w "Vless TLS" | cut -d: -f2|sed 's/ //g')"
 none="$(cat ~/log-install.txt | grep -w "Vless None TLS" | cut -d: -f2|sed 's/ //g')"
-until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
+until [[ ${user} =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "\\E[0;41;36m      Add Xray/Vless Account      \E[0m"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 
 		read -rp "User: " -e user
-		CLIENT_EXISTS=$(grep -w $user $xray/config.json | wc -l)
+		CLIENT_EXISTS=$(grep -w ${user} ${xray}/config.json | wc -l)
 
 		if [[ ${CLIENT_EXISTS} == '1' ]]; then
 clear
@@ -47,14 +47,14 @@ add-vless
 
 uuid=$(cat /proc/sys/kernel/random/uuid)
 read -p "Expired (days): " masaaktif
-exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
-sed -i '/#vless$/a\#vl# '"$user $exp"'\
-},{"id": "'""$uuid""'","email": "'""$user""'"' $xray/config.json
-sed -i '/#vlessgrpc$/a\#vl# '"$user $exp"'\
-},{"id": "'""$uuid""'","email": "'""$user""'"' $xray/config.json
-vlesslink1="vless://${uuid}@${domain}:$tls?path=/vless&security=tls&encryption=none&type=ws#${user}"
+exp=`date -d "${masaaktif} days" +"%Y-%m-%d"`
+sed -i '/#vless$/a\#vl# '"${user} ${exp}"'\
+},{"id": "'""${uuid}""'","email": "'""${user}""'"' ${xray}/config.json
+sed -i '/#vlessgrpc$/a\#vl# '"${user} ${exp}"'\
+},{"id": "'""${uuid}""'","email": "'""${user}""'"' ${xray}/config.json
+vlesslink1="vless://${uuid}@${domain}:${tls}?path=/vless&security=tls&encryption=none&type=ws#${user}"
 vlesslink2="vless://${uuid}@${domain}:$none?path=/vless&encryption=none&type=ws#${user}"
-vlesslink3="vless://${uuid}@${domain}:$tls?mode=gun&security=tls&encryption=none&type=grpc&serviceName=vless-grpc&sni=bug.com#${user}"
+vlesslink3="vless://${uuid}@${domain}:${tls}?mode=gun&security=tls&encryption=none&type=grpc&serviceName=vless-grpc&sni=bug.com#${user}"
 systemctl restart xray
 clear
 echo -e "----------------------------------" | tee -a /etc/log-create-user.log
@@ -78,7 +78,7 @@ echo -e "Link none TLS : ${vlesslink2}" | tee -a /etc/log-create-user.log
 echo -e "----------------------------------" | tee -a /etc/log-create-user.log
 echo -e "Link GRPC : ${vlesslink3}" | tee -a /etc/log-create-user.log
 echo -e "----------------------------------" | tee -a /etc/log-create-user.log
-echo -e "Expired On : $exp" | tee -a /etc/log-create-user.log
+echo -e "Expired On : ${exp}" | tee -a /etc/log-create-user.log
 echo -e "----------------------------------" | tee -a /etc/log-create-user.log
 echo "" | tee -a /etc/log-create-user.log
 read -n 1 -s -r -p "Press any key to back on menu"

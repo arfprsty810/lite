8afbcf9abab1f7b38c28504f567b2728d9b1f38c
arfprsty810 <94728173+arfprsty810@users.noreply.github.com>
2023-07-10 16:49:32 +0700

    Update renew-vless.sh

--- xray/vless/renew-vless.sh
+++ xray/vless/renew-vless.sh
@@ -15,7 +15,7 @@ xray="/etc/xray"
 logxray="/var/log/xray"
 
 clear
-NUMBER_OF_CLIENTS=$(grep -c -E "^#vl# " "$xray/config.json")
+NUMBER_OF_CLIENTS=$(grep -c -E "^#vl# " "${xray}/config.json")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 		clear
         echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
@@ -35,31 +35,31 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^#vl# " "$xray/config.json")
     echo -e "\\E[0;41;36m            Renew Vless            \E[0m"
     echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
     echo ""
-  	grep -E "^#vl# " "$xray/config.json" | cut -d ' ' -f 2-3 | column -t | sort | uniq
+  	grep -E "^#vl# " "${xray}/config.json" | cut -d ' ' -f 2-3 | column -t | sort | uniq
     echo ""
     red "tap enter to go back"
     echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 	read -rp "Input Username : " user
-    if [ -z $user ]; then
+    if [ -z ${user} ]; then
     menu-vless
     else
     read -p "Expired (days): " masaaktif
-    exp=$(grep -wE "^#vl# $user" "$xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
+    exp=$(grep -wE "^#vl# ${user}" "${xray}/config.json" | cut -d ' ' -f 3 | sort | uniq)
     now=$(date +%Y-%m-%d)
-    d1=$(date -d "$exp" +%s)
-    d2=$(date -d "$now" +%s)
+    d1=$(date -d "${exp}" +%s)
+    d2=$(date -d "${now}" +%s)
     exp2=$(( (d1 - d2) / 86400 ))
-    exp3=$(($exp2 + $masaaktif))
-    exp4=`date -d "$exp3 days" +"%Y-%m-%d"`
-    sed -i "/#vl# $user/c\#vl# $user $exp4" $xray/config.json
+    exp3=$((${exp2} + ${masaaktif}))
+    exp4=`date -d "${exp3} days" +"%Y-%m-%d"`
+    sed -i "/#vl# ${user}/c\#vl# ${user} ${exp4}" ${xray}/config.json
     systemctl restart xray > /dev/null 2>&1
     clear
     echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
     echo " VLESS Account Was Successfully Renewed"
     echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
     echo ""
-    echo " Client Name : $user"
-    echo " Expired On  : $exp4"
+    echo " Client Name : ${user}"
+    echo " Expired On  : ${exp4}"
     echo ""
     echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
     echo ""

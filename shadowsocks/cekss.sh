56209de524685f8c332c59ab4aa5094b07094f04
arfprsty810 <94728173+arfprsty810@users.noreply.github.com>
2023-07-10 16:23:07 +0700

    Update cekss.sh

--- shadowsocks/cekss.sh
+++ shadowsocks/cekss.sh
@@ -24,12 +24,12 @@ echo "----------------------------------";
 for akun in "${data[@]}"
 do
 port=$(cat /etc/shadowsocks-libev/akun.conf | grep '^port_tls' | cut -d ' ' -f 2 | tr '\n' ' ' | awk '{print $'"$x"'}')
-jum=$(netstat -anp | grep ESTABLISHED | grep obfs-server | cut -d ':' -f 2 | grep -w $port | awk '{print $2}' | sort | uniq | nl)
-if [[ -z "$jum" ]]; then
+jum=$(netstat -anp | grep ESTABLISHED | grep obfs-server | cut -d ':' -f 2 | grep -w ${port} | awk '{print $2}' | sort | uniq | nl)
+if [[ -z "${jum}" ]]; then
 echo > /dev/null
 else
-echo " $akun - $port"
-echo "$jum";
+echo " ${akun} - ${port}"
+echo "${jum}";
 echo "----------------------------------"
 fi
 x=$(( "$x" + 1 ))
@@ -43,12 +43,12 @@ echo "----------------------------------";
 for akun in "${data[@]}"
 do
 port=$(cat /etc/shadowsocks-libev/akun.conf | grep '^port_http' | cut -d ' ' -f 2 | tr '\n' ' ' | awk '{print $'"$x"'}')
-jum=$(netstat -anp | grep ESTABLISHED | grep obfs-server | cut -d ':' -f 2 | grep -w $port | awk '{print $2}' | sort | uniq | nl)
-if [[ -z "$jum" ]]; then
+jum=$(netstat -anp | grep ESTABLISHED | grep obfs-server | cut -d ':' -f 2 | grep -w ${port} | awk '{print $2}' | sort | uniq | nl)
+if [[ -z "${jum}" ]]; then
 echo > /dev/null
 else
-echo " $akun - $port"
-echo "$jum";
+echo " ${akun} - ${port}"
+echo "${jum}";
 echo "----------------------------------"
 fi
 x=$(( "$x" + 1 ))

12956ae753c63deb5e966edb08ebb2d4bc9c2314
arfprsty810 <94728173+arfprsty810@users.noreply.github.com>
2023-07-10 16:37:31 +0700

    Update del-tr.sh

--- xray/trojan/del-tr.sh
+++ xray/trojan/del-tr.sh
@@ -17,16 +17,16 @@ logxray="/var/log/xray"
 trgo="/etc/arfvpn/trojan-go"
 logtrgo="/var/log/arfvpn/trojan-go"
 ipvps="/var/lib/arfvpn"
-source $ipvps/ipvps.conf
-if [[ "$IP" = "" ]]; then
-domain=$(cat $arfvpn/domain)
+source ${ipvps}/ipvps.conf
+if [[ "${IP}" = "" ]]; then
+domain=$(cat ${arfvpn}/domain)
 else
-domain=$IP
+domain=${IP}
 fi
 clear 
 
 clear
-NUMBER_OF_CLIENTS=$(grep -c -E "^#tr# " "$xray/config.json")
+NUMBER_OF_CLIENTS=$(grep -c -E "^#tr# " "${xray}/config.json")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 		echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
         echo -e "\\E[0;41;36m       Delete Trojan  Account        \E[0m"
@@ -45,26 +45,25 @@ menu-trojan
     echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
     echo "  User       Expired  " 
 	echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-	grep -E "^#tr# " "$xray/config.json" | cut -d ' ' -f 2-3 | column -t | sort | uniq
+	grep -E "^#tr# " "${xray}/config.json" | cut -d ' ' -f 2-3 | column -t | sort | uniq
     echo ""
     red "tap enter to go back"
     echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 	read -rp "Input Username : " user
-    if [ -z $user ]; then
+    if [ -z ${user} ]; then
     menu-trojan
     else
-    exp=$(grep -wE "^#tr# $user" "$xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
-    sed -i "/^#tr# $user $exp/,/^},{/d" $xray/config.json
-    #sed -i "/^,#trgo# $user $exp/,/^/d" $trgo/config.json
-    sed -i "/^#trgo# $user $exp/d" $trgo/akun.conf
-    sed -i '/^,"'"$user"'"$/d' $trgo/config.json
+    exp=$(grep -wE "^#tr# ${user}" "${xray}/config.json" | cut -d ' ' -f 3 | sort | uniq)
+    sed -i "/^#tr# ${user} ${exp}/,/^},{/d" ${xray}/config.json
+    sed -i "/^#trgo# ${user} ${exp}/d" ${trgo}/akun.conf
+    sed -i '/^,"'"${user}"'"$/d' ${trgo}/config.json
     systemctl restart xray > /dev/null 2>&1
     clear
     echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
     echo " Trojan  Account Deleted Successfully"
     echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-    echo " Client Name : $user"
-    echo " Expired On  : $exp"
+    echo " Client Name : ${user}"
+    echo " Expired On  : ${exp}"
     echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
     echo ""
     read -n 1 -s -r -p "Press any key to back on menu"

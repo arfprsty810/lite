dae1936a69376ec443762a593db3286c383dbfdb
arfprsty810 <94728173+arfprsty810@users.noreply.github.com>
2023-07-10 16:02:39 +0700

    Update nginx-server.sh

--- nginx/nginx-server.sh
+++ nginx/nginx-server.sh
@@ -17,10 +17,10 @@ arfvpn="/etc/arfvpn"
 nginx="/etc/nginx"
 vps="/home/vps/public_html"
 github="https://raw.githubusercontent.com/arfprsty810/lite/main"
-domain=$(cat $arfvpn/domain)
-DOMAIN2="s/domainxxx/$domain/g";
-IP=$(cat $arfvpn/IP)
-MYIP2="s/ipxxx/$IP/g";
+domain=$(cat ${arfvpn}/domain)
+DOMAIN2="s/domainxxx/${domain}/g";
+IP=$(cat ${arfvpn}/IP)
+MYIP2="s/ipxxx/${IP}/g";
 clear
 
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
@@ -35,28 +35,28 @@ clear
 apt install nginx php php-fpm php-cli php-mysql libxml-parser-perl -y
 systemctl stop nginx
 apt-get remove --purge apache apache* -y
-rm $nginx/sites-enabled/*
-rm $nginx/sites-available/*
-wget -O $nginx/nginx.conf "$github/nginx/nginx.conf"
-wget -O $nginx/conf.d/vps.conf "$github/nginx/vps.conf"
+rm ${nginx}/sites-enabled/*
+rm ${nginx}/sites-available/*
+wget -O ${nginx}/nginx.conf "${github}/nginx/nginx.conf"
+wget -O ${nginx}/conf.d/vps.conf "${github}/nginx/vps.conf"
 sed -i 's/listen = \/run\/php\/php7.2-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php/7.2/fpm/pool.d/www.conf
 useradd -m vps;
-mkdir -p $vps
-echo "<?php phpinfo() ?>" > $vps/info.php
-chown -R www-data:www-data $vps
-chmod -R g+rw $vps
-cd $vps
-wget -O $vps/index.html "$github/nginx/index.html"
+mkdir -p ${vps}
+echo "<?php phpinfo() ?>" > ${vps}/info.php
+chown -R www-data:www-data ${vps}
+chmod -R g+rw ${vps}
+cd ${vps}
+wget -O ${vps}/index.html "${github}/nginx/index.html"
 
 cd
 wget -O /usr/bin/cert https://raw.githubusercontent.com/arfprsty810/lite/main/cert/cert.sh && chmod +x /usr/bin/cert && sed -i -e 's/\r$//' /usr/bin/cert && cert
-sudo openssl dhparam -out $nginx/dhparam.pem 2048
+sudo openssl dhparam -out ${nginx}/dhparam.pem 2048
 
-cd $nginx
-wget -O $nginx/sites-available/$domain.conf "$github/nginx/domain.conf"
-sed -i "$MYIP2" $nginx/sites-available/$domain.conf
-sed -i "$DOMAIN2" $nginx/sites-available/$domain.conf
-sudo ln -s $nginx/sites-available/$domain.conf $nginx/sites-enabled
+cd ${nginx}
+wget -O ${nginx}/sites-available/${domain}.conf "${github}/nginx/domain.conf"
+sed -i "${MYIP2}" ${nginx}/sites-available/${domain}.conf
+sed -i "${DOMAIN2}" ${nginx}/sites-available/${domain}.conf
+sudo ln -s ${nginx}/sites-available/${domain}.conf ${nginx}/sites-enabled
 
 cd
 systemctl restart nginx
